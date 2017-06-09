265dc6b6b9b27c78b3769d9022587bc89d3013d4
Noah Weninger <noah.weninger@ibm.com>
2017-06-09 09:41:12 -0400

    Make build system aware of TR_Listener
    Signed-off-by: Noah Weninger <noah.weninger@ibm.com>

--- runtime/compiler/build/files/common.mk
+++ runtime/compiler/build/files/common.mk
@@ -372,7 +372,8 @@ JIT_PRODUCT_SOURCE_FILES+=\
     omr/compiler/runtime/OMRCodeCacheConfig.cpp \
     omr/compiler/runtime/OMRCodeCacheManager.cpp \
     omr/compiler/runtime/OMRCodeCacheMemorySegment.cpp \
-    omr/compiler/runtime/OMRRuntimeAssumptions.cpp
+    omr/compiler/runtime/OMRRuntimeAssumptions.cpp \
+    compiler/trj9/runtime/Listener.cpp
 
 -include $(JIT_MAKE_DIR)/files/extra.mk
 include $(JIT_MAKE_DIR)/files/host/$(HOST_ARCH).mk
