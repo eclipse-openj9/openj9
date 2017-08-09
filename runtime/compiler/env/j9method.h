ea95edd9c5185c7403574ca5f1f82a895bd21f19
Omer Sheikh <Omer.Sheikh@ibm.com>
2017-08-09 12:05:42 -0400

    Allocate the exception table contiguously
    The exeption table is expected to be allocated contiguously to have a
    correct exception table offset when performing an AOT compilation.
    
    This may be a temporary solution. Rewriting so that the parent class of
    TR_ResolvedJ9JAASServerMethod is TR_ResolvedRelocatableJ9Method may be
    a better solution, as that class is written to expect a contiguous
    exception table.
    
    Signed-off-by: Omer Sheikh <Omer.Sheikh@ibm.com>

--- runtime/compiler/env/j9method.h
+++ runtime/compiler/env/j9method.h
@@ -593,6 +593,7 @@ public:
    virtual bool isInterpreted() override;
    virtual void setRecognizedMethodInfo(TR::RecognizedMethod rm) override;
    virtual J9ClassLoader *getClassLoader() override;
+   virtual U_8 * allocateException(uint32_t numBytes, TR::Compilation *comp);
    TR_ResolvedJ9Method *getRemoteMirror() const { return _remoteMirror; }
 
 private:
