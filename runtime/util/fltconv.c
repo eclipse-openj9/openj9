e62c8772f668f5881538aeb3ddd1305f2de2edd9
Hang Shao <hangshao@ca.ibm.com>
2017-09-20 11:50:21 -0400

    Remove flsanity test suite and obsolete declarations from util_api.h
    1. flsanity test suite is obsolete, the directory /flsanity is removed. 
    2. Remove reference to flsanity and fdlibm from j9.build-info, j9.flags
    and jvm.xml.ftl.
    3. Remove ifdef J9VM_MATH_USE_FDLIBM_MATH_LIBRARY and the code wrapped
    inside.
    4. Remove helper functions declared in util_api.h that has no
    implementation.
    
    issue #99
    
    Signed-off-by: Hang Shao <hangshao@ca.ibm.com>

--- runtime/util/fltconv.c
+++ runtime/util/fltconv.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1991, 2014 IBM Corp. and others
+ * Copyright (c) 1991, 2017 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -35,15 +35,6 @@
 
 #if (defined(LINUXPPC) || defined(ARMGNU)) && defined(HARDHAT)
 #include "fltdmath.h"
-#endif
-
-#ifdef J9VM_MATH_USE_FDLIBM_MATH_LIBRARY
-#undef fmod
-#undef remainder
-#define fmod fdlibm_fmod
-#define remainder fdlibm_remainder
-extern double fmod (double, double);
-extern double remainder (double, double);
 #endif
 
 static int fltconv_indexLeadingOne32 (U_32 u32val);
