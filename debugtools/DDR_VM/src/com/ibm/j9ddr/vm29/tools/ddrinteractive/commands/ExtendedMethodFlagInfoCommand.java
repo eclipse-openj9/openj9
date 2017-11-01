8c154fc8909c20e521f6fbd8e576fce184fb029a
Graham Chapman <graham_chapman@ca.ibm.com>
2017-10-31 23:15:29 -0400

    Fix DDR break
    Previous merge broke DDR.
    
    Signed-off-by: Graham Chapman <graham_chapman@ca.ibm.com>

--- debugtools/DDR_VM/src/com/ibm/j9ddr/vm29/tools/ddrinteractive/commands/ExtendedMethodFlagInfoCommand.java
+++ debugtools/DDR_VM/src/com/ibm/j9ddr/vm29/tools/ddrinteractive/commands/ExtendedMethodFlagInfoCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2014 IBM Corp. and others
+ * Copyright (c) 2001, 2017 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -56,9 +56,6 @@ public class ExtendedMethodFlagInfoCommand extends Command
 		}
 		if (flags.anyBitsIn(J9Consts.J9_RAS_METHOD_TRIGGERING)) {
 			out.println("J9_RAS_METHOD_TRIGGERING");
-		}
-		if (flags.anyBitsIn(J9Consts.J9_JVMTI_METHOD_SELECTIVE_ENTRY_EXIT)) {
-			out.println("J9_JVMTI_METHOD_SELECTIVE_ENTRY_EXIT");
 		}
 	}
 
