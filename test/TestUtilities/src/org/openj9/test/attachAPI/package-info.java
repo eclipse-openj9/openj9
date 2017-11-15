b714d0ac6d6d7364a69de2f2e65a9ac53842f0fa
Peter Bain <peter_bain@ca.ibm.com>
2017-11-15 09:06:29 -0500

    Add TestUtilities directory
    This creates a common directory to hold code shared amongst test projects and
    updates affected test projects' build.xml to use same.  This directory should
    be included as a source directory when compiling.  Code must be compilable by
    the lowest Java specification supported by OpenJ9.
    
    Signed-off-by: Peter Bain <peter_bain@ca.ibm.com>

---
+++ test/TestUtilities/src/org/openj9/test/attachAPI/package-info.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2017, 2017 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/
+ * or the Apache License, Version 2.0 which accompanies this distribution and
+ * is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following
+ * Secondary Licenses when the conditions for such availability set
+ * forth in the Eclipse Public License, v. 2.0 are satisfied: GNU
+ * General Public License, version 2 with the GNU Classpath
+ * Exception [1] and GNU General Public License, version 2 with the
+ * OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *******************************************************************************/
+
+/**
+ * This project contains common classes used by one or more testing projects.
+ * All code must be compilable at the lowest Java language level supported by OpenJ9.
+ */
+
+package org.openj9.test.attachAPI;
