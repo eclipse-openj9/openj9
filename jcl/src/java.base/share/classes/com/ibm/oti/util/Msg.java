8fc87280857fe06e0d91409abce26e751c1dc6ae
Peter Shipton <Peter_Shipton@ca.ibm.com>
2017-12-22 00:26:36 -0500

    Update the SPDX-License-Identifier as per the Eclipse OpenJ9 CQ
    Signed-off-by: Peter Shipton <Peter_Shipton@ca.ibm.com>

--- jcl/src/java.base/share/classes/com/ibm/oti/util/Msg.java
+++ jcl/src/java.base/share/classes/com/ibm/oti/util/Msg.java
@@ -21,7 +21,7 @@ package com.ibm.oti.util;
  * [1] https://www.gnu.org/software/classpath/license.html
  * [2] http://openjdk.java.net/legal/assembly-exception.html
  *
- * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
 import java.security.AccessController;
--- runtime/compiler/trj9/z/runtime/Math.m4
+++ runtime/compiler/trj9/z/runtime/Math.m4
@@ -23,7 +23,7 @@ ZZ
 ZZ [1] https://www.gnu.org/software/classpath/license.html
 ZZ [2] http://openjdk.java.net/legal/assembly-exception.html
 ZZ
-ZZ SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ZZ SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
 
 
 changequote([,])dnl
