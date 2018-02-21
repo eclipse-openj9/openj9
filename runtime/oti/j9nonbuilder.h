eclipse-openj9/openj9

commit 9a88388c32389458b98adb3bfa26ab882f59cf53

Add ACC_VALUE_TYPE class access modifier for Valhalla L-World
Signed-off-by: Eric Zhang <eric99.zhang@gmail.com>

runtime/oti/j9nonbuilder.h
@@ -140,6 +140,9 @@
 #define J9AccSynchronized 0x20
 #define J9AccSynthetic 0x1000
 #define J9AccTransient 0x80
+#if defined(J9VM_OPT_VALHALLA_VALUE_TYPES)
+#define J9AccValueType 0x100
+#endif /* defined(J9VM_OPT_VALHALLA_VALUE_TYPES) */
 #define J9AccVarArgs 0x80
 #define J9AccVolatile 0x40
 #define J9StaticFieldRefBaseType 0x1

runtime/oti/cfr.h
@@ -827,6 +827,9 @@ typedef struct J9CfrClassFile {
 #define CFR_ACC_BRIDGE  					0x00000040
 #define CFR_ACC_VOLATILE  					0x00000040
 #define CFR_ACC_TRANSIENT  					0x00000080
+#if defined(J9VM_OPT_VALHALLA_VALUE_TYPES)
+#define CFR_ACC_VALUE_TYPE					0x00000100
+#endif /* defined(J9VM_OPT_VALHALLA_VALUE_TYPES) */
 #define CFR_ACC_VARARGS  					0x00000080
 #define CFR_ACC_NATIVE  					0x00000100
 #define CFR_ACC_INTERFACE  					0x00000200
@@ -858,7 +861,11 @@ typedef struct J9CfrClassFile {
 #define CFR_MAJOR_VERSION  45
 #define CFR_MINOR_VERSION  3
 #define CFR_PUBLIC_PRIVATE_PROTECTED_MASK	(CFR_ACC_PUBLIC | CFR_ACC_PRIVATE | CFR_ACC_PROTECTED)
+#if defined(J9VM_OPT_VALHALLA_VALUE_TYPES)
+#define CFR_CLASS_ACCESS_MASK					(CFR_ACC_PUBLIC | CFR_ACC_FINAL | CFR_ACC_SUPER | CFR_ACC_INTERFACE | CFR_ACC_ABSTRACT | CFR_ACC_SYNTHETIC | CFR_ACC_ANNOTATION | CFR_ACC_ENUM | CFR_ACC_VALUE_TYPE)
+#else /* defined(J9VM_OPT_VALHALLA_VALUE_TYPES) */
 #define CFR_CLASS_ACCESS_MASK					(CFR_ACC_PUBLIC | CFR_ACC_FINAL | CFR_ACC_SUPER | CFR_ACC_INTERFACE | CFR_ACC_ABSTRACT | CFR_ACC_SYNTHETIC | CFR_ACC_ANNOTATION | CFR_ACC_ENUM)
+#endif /* defined(J9VM_OPT_VALHALLA_VALUE_TYPES) */
 #define CFR_INTERFACE_CLASS_ACCESS_MASK			(CFR_ACC_PUBLIC | CFR_ACC_INTERFACE | CFR_ACC_ABSTRACT | CFR_ACC_SYNTHETIC | CFR_ACC_ANNOTATION)
 #define CFR_FIELD_ACCESS_MASK  					(CFR_ACC_PUBLIC | CFR_ACC_PRIVATE | CFR_ACC_PROTECTED | CFR_ACC_STATIC | CFR_ACC_FINAL | CFR_ACC_VOLATILE | CFR_ACC_TRANSIENT | CFR_ACC_SYNTHETIC | CFR_ACC_ENUM)
 #define CFR_INTERFACE_FIELD_ACCESS_MASK  		(CFR_ACC_PUBLIC | CFR_ACC_STATIC | CFR_ACC_FINAL | CFR_ACC_SYNTHETIC)

commit a9d24b90bcc8212b268f2f7a4a43bc06795ed0b1

Remove #if defined in j9nonbuilder.h to fix DDR compile error
Resolves DDR compile error introduced in:
9a88388c32389458b98adb3bfa26ab882f59cf53

Signed-off-by: Eric Zhang <eric99.zhang@gmail.com>

runtime/oti/j9nonbuilder.h
@@ -139,9 +139,7 @@
 #define J9AccSynchronized 0x20
 #define J9AccSynthetic 0x1000
 #define J9AccTransient 0x80
-#if defined(J9VM_OPT_VALHALLA_VALUE_TYPES)
 #define J9AccValueType 0x100
-#endif /* defined(J9VM_OPT_VALHALLA_VALUE_TYPES) */
 #define J9AccVarArgs 0x80
 #define J9AccVolatile 0x40
 #define J9StaticFieldRefBaseType 0x1