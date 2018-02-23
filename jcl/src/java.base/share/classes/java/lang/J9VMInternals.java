96e34a39d87c2b152a02001c234868a32c604933
Dan Heidinga <daniel_heidinga@ca.ibm.com>
2018-02-23 15:32:30 -0500

    Change fastIdentityHashCode() to use unsigned int to long conversion
    J9VMInternals.fastIdentityHashCode(Object) incorrectly sign-extended the
    32bit J9Class pointer when reading it from an object header in a
    compressedrefs VM.
    
    This can can cause crashes with i.e. InaccessibleReadAddress=FFFFFFFF999609D8
    in Java_com_ibm_jit_JITHelpers_getBackfillOffsetFromJ9Class64.
    
    The int value needs to be assigned to the long without sign-extension.
    Code now uses Integer.toUnsignedLong(int) to work around the issue.
    
    JShell example showing this is valid:
    ---
    jshell> int ptr = 0x99960900
    ptr ==> -1718220544
    
    jshell> long lptr = Integer.toUnsignedLong(ptr)
    lptr ==> 2576746752
    
    jshell> Long.toHexString(lptr)
    $3 ==> "99960900"
    
    jshell> long castptr = ptr
    castptr ==> -1718220544
    
    jshell> Long.toHexString(castptr)
    $5 ==> "ffffffff99960900"
    ---
    
    Signed-off-by: Dan Heidinga <daniel_heidinga@ca.ibm.com>

--- jcl/src/java.base/share/classes/java/lang/J9VMInternals.java
+++ jcl/src/java.base/share/classes/java/lang/J9VMInternals.java
@@ -30,7 +30,7 @@ import com.ibm.oti.util.Msg;
 
 
 /*******************************************************************************
- * Copyright (c) 1998, 2017 IBM Corp. and others
+ * Copyright (c) 1998, 2018 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -439,7 +439,7 @@ final class J9VMInternals {
 				}
 			}
 		} else {
-			long ptr = (com.ibm.oti.vm.VM.FJ9OBJECT_SIZE == 4) ?  h.getIntFromObject(anObject, 0L) : h.getLongFromObject(anObject, 0L);
+			long ptr = (com.ibm.oti.vm.VM.FJ9OBJECT_SIZE == 4) ? Integer.toUnsignedLong(h.getIntFromObject(anObject, 0L)) : h.getLongFromObject(anObject, 0L);
 			if ((ptr & com.ibm.oti.vm.VM.OBJECT_HEADER_HAS_BEEN_MOVED_IN_CLASS) != 0) {
 				if (!h.isArray(anObject)) {
 					long j9class = ptr & com.ibm.oti.vm.VM.J9_JAVA_CLASS_MASK;
