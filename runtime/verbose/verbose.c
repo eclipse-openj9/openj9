50c0f81d5110ce41bde773e5c596008d701449dd
Keith W. Campbell <keithc@ca.ibm.com>
2018-09-20 16:09:25 -0400

    Remove verbose.h
    After removing all the duplicate definitions from verbose.h,
    it would only include verbose_api.h; by adjusting the clients
    to include verbose_api.h, verbose.h can simply be removed.
    
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- runtime/verbose/verbose.c
+++ runtime/verbose/verbose.c
@@ -20,11 +20,10 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
-
 #include <string.h>
 
 #include "j9.h"
-#include "verbose.h"
+#include "verbose_api.h"
 #include "omrlinkedlist.h"
 #include "VerboseGCInterface.h"
 #ifdef J9VM_INTERP_NATIVE_SUPPORT
