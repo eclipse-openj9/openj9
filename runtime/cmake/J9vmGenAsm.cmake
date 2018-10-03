2af3f22116bb1d7c8f09749a35ed9f52c4153ddd
Devin Nakamura <devinn@ca.ibm.com>
2018-10-03 11:20:19 -0400

    CMake: Add j9vm_gen_asm function
    Function simplifies generating assembly from m4 files
    
    Signed-off-by: Devin Nakamura <devinn@ca.ibm.com>

--- runtime/cmake/J9vmGenAsm.cmake
+++ runtime/cmake/J9vmGenAsm.cmake
@@ -1,5 +1,5 @@
 ################################################################################
-# Copyright (c) 2017, 2018 IBM Corp. and others
+# Copyright (c) 2018, 2018 IBM Corp. and others
 #
 # This program and the accompanying materials are made available under
 # the terms of the Eclipse Public License 2.0 which accompanies this
@@ -20,55 +20,35 @@
 # SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
 ################################################################################
 
-add_tracegen(j9codertvm.tdf)
-add_library(j9codert_vm STATIC 
-	cache.c
-	cnathelp.cpp
-	CodertVMHelpers.cpp
-	decomp.cpp
-	dlt.c
-	fsdcsup.c
-	jitavl.c
-	jithash.cpp
-	jsr292.c
-	jswalk.c
-	thunkcrt.c
-
-	${CMAKE_CURRENT_BINARY_DIR}/ut_j9codertvm.c
-)
-
-target_include_directories(j9codert_vm
-    PRIVATE
-    .
-	${CMAKE_CURRENT_BINARY_DIR}
-    ${j9vm_SOURCE_DIR}/gc_include
-    ${omr_SOURCE_DIR}/gc/include
-	${j9vm_SOURCE_DIR}/compiler/runtime/
-)
-
-if(OMR_ARCH_X86)
-  j9vm_gen_asm(xnathelp.m4)
-
-  # conditionalize this in the future
-  target_sources(j9codert_vm PRIVATE
-	  xnathelp.s
-  )
-elseif(OMR_ARCH_POWER)
-  j9vm_gen_asm(pnathelp.m4)
-
-  # conditionalize this in the future
-  target_sources(j9codert_vm PRIVATE
-	  pnathelp.s
-  )
+# Include once
+if(COMMAND j9vm_gen_asm)
+	return()
 endif()
 
+# Helper function for generating assembly from m4
+# usage: j9vm_gen_asm(some_file.m4 ....)
+# converts 'some_file.m4' into 'some_file.s'.
+# Note: path is interpreted relative to CMAKE_CURRENT_SOURCE_DIR
+# .s file is put in CMAKE_CURRENT_BINARY_DIR 
+# Directory names from the input are ignored. IE. foo/bar.m4 > bar.s 
+function(j9vm_gen_asm)
+	set(m4_defines "")
+	if(OMR_HOST_OS STREQUAL "linux")
+		set(m4_defines "-DLINUX")
+	elseif(OMR_HOST_OS STREQUAL "osx")
+		set(m4_defines "-DOSX")
+	endif()
+	#TODO need to add other platforms here
 
-if(J9VM_JIT_RUNTIME_INSTRUMENTATION)
-  target_sources(j9codert_vm PRIVATE runtimeInstrumentation.c)
-endif()
+	foreach(m4_file IN LISTS ARGN)
+		get_filename_component(base_name "${m4_file}" NAME_WE)
+		
+		add_custom_command(
+			OUTPUT ${base_name}.s
+			DEPENDS ${m4_file} run_constgen
+			COMMAND m4 -I "${j9vm_SOURCE_DIR}/oti" ${m4_defines} ${CMAKE_CURRENT_SOURCE_DIR}/${m4_file} > ${base_name}.s
+		)
+	endforeach()
 
-target_link_libraries(j9codert_vm
-	PRIVATE
-		j9vm_interface
-		j9vm_compiler_defines
-)
+
+endfunction(j9vm_gen_asm)
