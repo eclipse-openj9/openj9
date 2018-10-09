7d277b0810732b030a050afa00c1ae041bfa6f84
Aleksandar Micic <amicic@ca.ibm.com>
2018-10-09 15:14:06 -0400

    Revert "CopyForwardHybrid"

--- runtime/gc_vlhgc/HeapRegionDescriptorVLHGC.cpp
+++ runtime/gc_vlhgc/HeapRegionDescriptorVLHGC.cpp
@@ -1,6 +1,6 @@
 
 /*******************************************************************************
- * Copyright (c) 1991, 2018 IBM Corp. and others
+ * Copyright (c) 1991, 2014 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -70,7 +70,6 @@ MM_HeapRegionDescriptorVLHGC::initialize(MM_EnvironmentBase *env, MM_HeapRegionM
 	}
 
 	_markData._shouldMark = false;
-	_markData._noEvacuation = false;
 	_markData._dynamicMarkCost = 0;
 	_markData._overflowFlags = 0x0;
 	_reclaimData._shouldReclaim = false;
