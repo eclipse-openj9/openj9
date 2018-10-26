a8b4ec2b76fb9ab52662cbb7e7691000218ebacf
Graham Chapman <graham_chapman@ca.ibm.com>
2018-10-26 08:48:44 -0400

    Halt thread before getting owned monitor data
    - halt target thread in GetOwnedMonitorStackDepthInfo
    - add test for targetting non-current thread
    
    Note the test validates nothing - it just calls the API repeatedly on a
    foreign thread. This crashes on unfixed VMs.
    
    Fixes: #3400
    
    [ci skip]
    
    Signed-off-by: Graham Chapman <graham_chapman@ca.ibm.com>

--- runtime/jvmti/jvmtiThread.c
+++ runtime/jvmti/jvmtiThread.c
@@ -597,6 +597,8 @@ jvmtiGetOwnedMonitorStackDepthInfo(jvmtiEnv* env,
 			J9ObjectMonitorInfo * monitorEnterRecords = NULL;
 			jvmtiMonitorStackDepthInfo * resultArray = NULL;
 
+			vm->internalVMFunctions->haltThreadForInspection(currentThread, targetThread);
+
 			/* Get the count of owned monitors */
 
 			maxRecords = vm->internalVMFunctions->getOwnedObjectMonitors(currentThread, targetThread, NULL, 0);
@@ -660,6 +662,7 @@ doneRelease:
 				j9mem_free_memory(monitorEnterRecords);
 			}
 
+			vm->internalVMFunctions->resumeThreadForInspection(currentThread, targetThread);
 			releaseVMThread(currentThread, targetThread);
 		}
 done:
