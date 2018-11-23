ee66ae17dd57c8d392f2fbb053b8b7dddecf2cbc
Victor Ding <dvictor@ca.ibm.com>
2018-11-23 15:11:57 -0500

    Remove deprecated snippets on X86 [Phase 1/2]
    Following snippets are deprecated, cleaning up.
    
    X86StackOverflowCheckSnippet
    IA32WriteBarrierSnippet
    AMD64WriteBarrierSnippet
    X86JNIPauseSnippet
    X86PassJNINullSnippet
    
    Note that this commit still keeps some of deprecated files,
    a subsequent commit will remove them completely.
    
    Signed-off-by: Victor Ding <dvictor@ca.ibm.com>

--- runtime/compiler/x/codegen/PassJNINullSnippet.cpp
+++ runtime/compiler/x/codegen/PassJNINullSnippet.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2016 IBM Corp. and others
+ * Copyright (c) 2000, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -20,41 +20,15 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
-#include "x/codegen/PassJNINullSnippet.hpp"
-#include "env/IO.hpp"
+ //  __   ___  __   __   ___  __       ___  ___  __
+ // |  \ |__  |__) |__) |__  /  `  /\   |  |__  |  \
+ // |__/ |___ |    |  \ |___ \__, /~~\  |  |___ |__/
+ //
+ // This file is now deprecated and will be deleted soon.
 
-uint8_t *TR::X86PassJNINullSnippet::emitSnippetBody()
-   {
-   uint8_t *buffer = cg()->getBinaryBufferCursor();
-   getSnippetLabel()->setCodeLocation(buffer);
+#include "ras/Debug.hpp"
 
-   *buffer++ = 0x6a; // push   imms
-   *buffer++ = 0;
-
-   return genRestartJump(buffer);
-   }
-
-
-void
-TR_Debug::print(TR::FILE *pOutFile, TR::X86PassJNINullSnippet  * snippet)
-   {
-   if (pOutFile == NULL)
-      return;
-
-   uint8_t *bufferPos = snippet->getSnippetLabel()->getCodeLocation();
-
-   printSnippetLabel(pOutFile, snippet->getSnippetLabel(), bufferPos, getName(snippet));
-
-   printPrefix(pOutFile, NULL, bufferPos, 2);
-   trfprintf(pOutFile, "push\t0\t%s push explicit JNI NULL",
-                 commentString());
-   bufferPos += 2;
-
-   printRestartJump(pOutFile, snippet, bufferPos);
-   }
-
-
-uint32_t TR::X86PassJNINullSnippet::getLength(int32_t estimatedSnippetStart)
-   {
-   return 2 + estimateRestartJumpLength(estimatedSnippetStart + 2);
-   }
+#ifdef BUILD_DEPRECATED_TR_DEBUG_PRINT
+namespace TR { class X86PassJNINullSnippet; }
+void TR_Debug::print(TR::FILE *pOutFile, TR::X86PassJNINullSnippet* snippet) { }
+#endif
