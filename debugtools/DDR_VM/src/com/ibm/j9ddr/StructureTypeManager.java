55e7cd4c8c5cc700f3048fd316201eb55a908e6f
Keith W. Campbell <keithc@ca.ibm.com>
2019-03-14 16:44:37 -0400

    Fix handling of 'volatile' qualifier
    * there isn't necessarily a space after 'const' or 'volatile'
      e.g. the owner field of a monitor has type 'J9Thread*volatile'
    
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- debugtools/DDR_VM/src/com/ibm/j9ddr/StructureTypeManager.java
+++ debugtools/DDR_VM/src/com/ibm/j9ddr/StructureTypeManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2018 IBM Corp. and others
+ * Copyright (c) 2010, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -156,7 +156,7 @@ public class StructureTypeManager
 		}
 	}
 
-	private static final Pattern CVQualifierPattern = Pattern.compile("\\s*\\b(const|volatile)\\s+");
+	private static final Pattern CVQualifierPattern = Pattern.compile("\\s*\\b(const|volatile)\\b\\s*");
 
 	public int getType(String rawType)
 	{
