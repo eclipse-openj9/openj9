d50e93be1d91d3f444c45fa772775f9900a3b0cd
Dan Heidinga <daniel_heidinga@ca.ibm.com>
2019-04-04 02:37:39 -0400

    Update formatting in bcverify.c
    Signed-off-by: Dan Heidinga <daniel_heidinga@ca.ibm.com>

--- runtime/bcverify/bcverify.c
+++ runtime/bcverify/bcverify.c
@@ -1708,11 +1708,11 @@ simulateStack (J9BytecodeVerificationData * verifyData)
 			stackTop -= getSendSlotsFromSignature(J9UTF8_DATA(utf8string));
 
 			if ((JBinvokestatic != bc) 
-				&& (JBinvokedynamic != bc)
-				&& (JBinvokestaticsplit != bc)
+			&& (JBinvokedynamic != bc)
+			&& (JBinvokestaticsplit != bc)
 			) {
 				if ((JBinvokespecial == bc) 
-					|| (JBinvokespecialsplit == bc)
+				|| (JBinvokespecialsplit == bc)
 				) {
 
 					type = POP;
@@ -2290,7 +2290,7 @@ j9bcv_initializeVerificationData(J9JavaVM* javaVM)
 	J9BytecodeVerificationData * verifyData;
 	PORT_ACCESS_FROM_JAVAVM(javaVM);
 	JavaVM* jniVM = (JavaVM*)javaVM;
-    J9ThreadEnv* threadEnv;
+	J9ThreadEnv* threadEnv;
 
 	(*jniVM)->GetEnv(jniVM, (void**)&threadEnv, J9THREAD_VERSION_1_1);
 
@@ -2430,8 +2430,9 @@ j9bcv_verifyBytecodes (J9PortLibrary * portLib, J9Class * clazz, J9ROMClass * ro
 		if (!((romMethod->modifiers & J9AccNative) || (romMethod->modifiers & J9AccAbstract))) {
 			BOOLEAN isInitMethod = FALSE;
 
-	        /* BCV_TARGET_STACK_HEADER_UDATA_SIZE for pc/stackBase/stackEnd in J9BranchTargetStack and */
-	        /* BCV_STACK_OVERFLOW_BUFFER_UDATA_SIZE for late overflow detection of longs/doubles */
+			/* BCV_TARGET_STACK_HEADER_UDATA_SIZE for pc/stackBase/stackEnd in J9BranchTargetStack and
+			 * BCV_STACK_OVERFLOW_BUFFER_UDATA_SIZE for late overflow detection of longs/doubles
+			 */
 			verifyData->stackSize = (J9_MAX_STACK_FROM_ROM_METHOD(romMethod)
 									+ J9_ARG_COUNT_FROM_ROM_METHOD(romMethod)
 									+ J9_TEMP_COUNT_FROM_ROM_METHOD(romMethod)
@@ -2452,7 +2453,6 @@ _fallBack:
 			createStackMaps = !classVersionRequiresStackmaps && (verifyData->ignoreStackMaps || !hasStackMaps);
 
 			if (createStackMaps) {
-				
 				verifyData->stackMapsCount = buildBranchMap(verifyData);
 				
 				if (verifyData->stackMapsCount == (UDATA)BCV_ERR_INTERNAL_ERROR) {
