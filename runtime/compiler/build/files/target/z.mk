5bdfc889340d1a4b2c3d0480e57749041439de36
Filip Jeremic <fjeremic@ca.ibm.com>
2019-04-09 17:35:59 -0400

    Create PPA1 snippet
    Signed-off-by: Filip Jeremic <fjeremic@ca.ibm.com>

--- runtime/compiler/build/files/target/z.mk
+++ runtime/compiler/build/files/target/z.mk
@@ -51,6 +51,7 @@ JIT_PRODUCT_BACKEND_SOURCES+=\
     omr/compiler/z/codegen/SystemLinkage.cpp \
     omr/compiler/z/codegen/TranslateEvaluator.cpp \
     omr/compiler/z/codegen/UnaryEvaluator.cpp \
+    omr/compiler/z/codegen/snippet/PPA1Snippet.cpp \
     omr/compiler/z/env/OMRCPU.cpp
 
 JIT_PRODUCT_SOURCE_FILES+=\
