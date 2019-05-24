6109075096959a9a54b9ad0185adac5b5859841e
Bohao(Aaron) Wang <aaronwang0407@gmail.com>
2019-05-24 10:24:01 -0400

    Move _dummyTempStorageRefNode to OpenJ9
    Move initialization and declaration of variable _dummyTempStorageRefNode to J9::CodeGenerator.
    
    Issue: eclipse/omr#1897
    Signed-off-by: Bohao(Aaron) Wang <aaronwang0407@gmail.com>

--- runtime/compiler/codegen/J9CodeGenerator.hpp
+++ runtime/compiler/codegen/J9CodeGenerator.hpp
@@ -331,6 +331,11 @@ private:
 
    TR_BitVector *_liveMonitors;
 
+protected:
+
+   // isTemporaryBased storageReferences just have a symRef but some other routines expect a node so use the below to fill in this symRef on this node
+   TR::Node *_dummyTempStorageRefNode;
+
 public:
 
    static bool wantToPatchClassPointer(TR::Compilation *comp,
