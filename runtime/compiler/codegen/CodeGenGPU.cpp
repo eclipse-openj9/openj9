a334076cae497a29e36d9cd1d9239013ca0c594a
Rounak Agarwal <rounakr.ag73@gmail.com>
2019-06-14 10:48:38 +0530

    Remove unused TR::bztestnset opcode
    Fixes #6107
    
    Signed-off-by: Rounak Agarwal <rounakr.ag73@gmail.com>

--- runtime/compiler/codegen/CodeGenGPU.cpp
+++ runtime/compiler/codegen/CodeGenGPU.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corp. and others
+ * Copyright (c) 2000, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -749,7 +749,6 @@ static const char * nvvmOpCodeNames[] =
    NULL,          // TR::lusubb
    NULL,          // TR::icmpset
    NULL,          // TR::lcmpset
-   NULL,          // TR::bztestnset
    NULL,          // TR::ibatomicor
    NULL,          // TR::isatomicor
    NULL,          // TR::iiatomicor
