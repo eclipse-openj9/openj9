db8ee6b920bdb65e51f87c53e35b8b24fafc7c35
simonameng <simonameng97@gmail.com>
2019-06-20 16:54:20 -0400

    Change the copyright
    Change the copyright to 2019
    
    Signed-off-by: simonameng <simonameng97@gmail.com>

--- runtime/compiler/optimizer/J9RecognizedCallTransformer.hpp
+++ runtime/compiler/optimizer/J9RecognizedCallTransformer.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2017, 2018 IBM Corp. and others
+* Copyright (c) 2017, 2019 IBM Corp. and others
 *
 * This program and the accompanying materials are made available under
 * the terms of the Eclipse Public License 2.0 which accompanies this
