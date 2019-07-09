868b440ead22f7ec8d82e048c853159fa1c2b444
Keith W. Campbell <keithc@ca.ibm.com>
2019-07-09 17:24:02 -0400

    Improve Java preprocessor
    Extend expression language used in 'INCLUDE-IF' and 'IF' preprocessor
    directives to permit comparisons (<, <=, ==, !=, >=, >) between numeric
    values expressed as decimal literals or references to macros with
    integer values.
    
    The primary envisioned use-case is to replace existing JavaNN flags
    with conditions like 'JAVA_SPEC_VERSION >= NN'.
    
    * JavaPreprocessor
      - add scanner and recursive-descent parser
      - remove setCopyrightYear() - it did nothing
      - change isOperatorOrBracket() to match its documentation
    
    * ConfigXMLHandler
      - clean up javadoc
      - remove useless calls to toString()
      - introduce and reuse local variables
    
    * General improvements
      - reuse local variables
      - remove redundant methods
      - fix javadoc formatting
      - use StringBuilder instead of StringBuffer
      - make nested classes private and final are decimal literals
    
    * add com.ibm.jpp.preprocessor to .classpath
    
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- sourcetools/com.ibm.jpp.preprocessor/com/ibm/jpp/om/Builder.java
+++ sourcetools/com.ibm.jpp.preprocessor/com/ibm/jpp/om/Builder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1999, 2019 IBM Corp. and others
+ * Copyright (c) 1999, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -347,7 +347,6 @@ public class Builder {
 						if (!updateAllCopyrights) {
 							cal.setTime(new Date(sourceFile.lastModified()));
 						}
-						jpp.setCopyrightYear(cal.get(Calendar.YEAR));
 						jpp.addValidFlags(registry.getValidFlags());
 						/*[PR 120411] Use a javadoc tag instead of TestBootpath preprocessor tag*/
 						jpp.setTestBootPath(isTestsBootPath);
