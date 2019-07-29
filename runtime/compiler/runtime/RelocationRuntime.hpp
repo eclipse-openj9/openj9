dcca397363e606bd393ac2ff4038769b3b42c9e0
Younes Manton <ymanton@ca.ibm.com>
2019-07-29 14:23:11 -0400

    Remove unused isMethodInSharedCache
    isMethodInSharedCache is unused and not particularly useful anyway.
    
    Signed-off-by: Younes Manton <ymanton@ca.ibm.com>

--- runtime/compiler/runtime/RelocationRuntime.hpp
+++ runtime/compiler/runtime/RelocationRuntime.hpp
@@ -191,7 +191,6 @@ class TR_RelocationRuntime {
 
       virtual void *isROMClassInSharedCaches(UDATA romClassValue);
       virtual bool isRomClassForMethodInSharedCache(J9Method *method);
-      virtual TR_YesNoMaybe isMethodInSharedCache(J9Method *method);
       virtual TR_OpaqueClassBlock *getClassFromCP(J9VMThread *vmThread, J9ConstantPool *constantPool, I_32 cpIndex, bool isStatic);
 
       static uintptr_t    getGlobalValue(uint32_t g)
@@ -363,7 +362,6 @@ public:
 
       virtual void *isROMClassInSharedCaches(UDATA romClassValue);
       virtual bool isRomClassForMethodInSharedCache(J9Method *method);
-      virtual TR_YesNoMaybe isMethodInSharedCache(J9Method *method);
 
       virtual TR_OpaqueClassBlock *getClassFromCP(J9VMThread *vmThread, J9ConstantPool *constantPool, I_32 cpIndex, bool isStatic);
 
