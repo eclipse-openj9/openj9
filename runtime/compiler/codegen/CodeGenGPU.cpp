d6d7b617e4ff190a52f8604269beb425e0d9bd7d
Bohao(Aaron) Wang <aaronwang0407@gmail.com>
2019-08-20 10:21:26 -0400

    Add FMA ILopcodes support in OpenJ9
    Add Opcode support from OpenJ9 for FMA opcodes. Add NULL to
    nvvmOpCodeNames list in CodeGenGPU.cpp for the 8 FMA opcodes.
    
    Signed-off-by: Bohao(Aaron) Wang <aaronwang0407@gmail.com>

--- runtime/compiler/codegen/CodeGenGPU.cpp
+++ runtime/compiler/codegen/CodeGenGPU.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corp. and others
+ * Copyright (c) 2000, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -208,6 +208,16 @@ static const char * nvvmOpCodeNames[] =
    "xor",         // TR::lxor
    "xor",         // TR::bxor
    "xor",         // TR::sxor
+
+   NULL,          // TR::fmuladd
+   NULL,          // TR::fmulsub
+   NULL,          // TR::fnegmuladd
+   NULL,          // TR::fnegmulsub
+   NULL,          // TR::dmuladd
+   NULL,          // TR::dmulsub
+   NULL,          // TR::dnegmuladd
+   NULL,          // TR::dnegmulsub
+
    "sext",        // TR::i2l
    "sitofp",      // TR::i2f
    "sitofp",      // TR::i2d
