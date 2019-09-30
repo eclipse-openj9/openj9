974082f8dd28aae646a0ce55ecb3dd7f379f4907
Bohao(Aaron) Wang <aaronwang0407@gmail.com>
2019-09-30 11:25:37 -0400

    remove TR::imulover IL opcode
    Signed-off-by: Bohao(Aaron) Wang <aaronwang0407@gmail.com>

--- runtime/compiler/codegen/CodeGenGPU.cpp
+++ runtime/compiler/codegen/CodeGenGPU.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corp. and others
+ * Copyright (c) 2000, 2019 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -815,7 +815,6 @@ static const char * nvvmOpCodeNames[] =
 
 
 
-   NULL,          // TR::imulover
 
    NULL,          // TR::dfloor
    NULL,          // TR::ffloor
