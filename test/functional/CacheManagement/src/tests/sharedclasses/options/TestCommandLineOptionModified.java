b44cdbaa5804233a1b5b2b914e37d79d0478e297
Sophia Guo <sophiag@ca.ibm.com>
2019-11-06 10:40:10 -0500

    Add copyright
    Signed-off-by: Sophia Guo <sophiag@ca.ibm.com>

--- test/functional/CacheManagement/src/tests/sharedclasses/options/TestCommandLineOptionModified.java
+++ test/functional/CacheManagement/src/tests/sharedclasses/options/TestCommandLineOptionModified.java
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2019 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/
+ * or the Apache License, Version 2.0 which accompanies this distribution and
+ * is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following
+ * Secondary Licenses when the conditions for such availability set
+ * forth in the Eclipse Public License, v. 2.0 are satisfied: GNU
+ * General Public License, version 2 with the GNU Classpath
+ * Exception [1] and GNU General Public License, version 2 with the
+ * OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
 package tests.sharedclasses.options;
 
 import tests.sharedclasses.TestUtils;
