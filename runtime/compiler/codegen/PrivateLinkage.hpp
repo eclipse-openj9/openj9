58d01534c448d80aca34a32f4a2677a23df2ed2b
Daryl Maier <maier@ca.ibm.com>
2019-11-12 07:14:00 -0500

    Guard PrivateLinkage.hpp from multiple includes
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- runtime/compiler/codegen/PrivateLinkage.hpp
+++ runtime/compiler/codegen/PrivateLinkage.hpp
@@ -20,6 +20,9 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
+#ifndef J9_PRIVATELINKAGE_INCL
+#define J9_PRIVATELINKAGE_INCL
+
 #include "codegen/Linkage.hpp"
 
 namespace TR { class CodeGenerator; }
@@ -39,3 +42,5 @@ public:
    };
 
 }
+
+#endif
