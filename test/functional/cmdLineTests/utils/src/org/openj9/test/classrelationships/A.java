7e4110efed683207968521eee69c6105fd15d5ae
Sharon Wang <sharon-wang-cpsc@outlook.com>
2019-12-11 12:06:59 -0500

    Add tests for Class Relationship Snippets
    Check that snippets are being successfully stored during the first
    run of an application and that subsequent runs are skipping the
    linear bytecode walk accordingly.
    
    Signed-off-by: Sharon Wang <sharon-wang-cpsc@outlook.com>

---
+++ test/functional/cmdLineTests/utils/src/org/openj9/test/classrelationships/A.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2019, 2020 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/
+ * or the Apache License, Version 2.0 which accompanies this distribution and
+ * is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following
+ * Secondary Licenses when the conditions for such availability set
+ * forth in the Eclipse Public License, v. 2.0 are satisfied: GNU
+ * General Public License, version 2 with the GNU Classpath
+ * Exception [1] and GNU General Public License, version 2 with the
+ * OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+/**
+ * @file A.java
+ * @brief Class A for Class Relationship Verifier Snippets tests.
+ */
+
+package org.openj9.test.classrelationships;
+
+class A {
+	public static void main(String [] args) {
+		System.out.println("Running org.openj9.test.classrelationships.A");
+	}
+
+	public void passTypeBtoC(B b) {
+		acceptTypeC(b);
+	}
+
+	public void acceptTypeC(C c) {}
+}
