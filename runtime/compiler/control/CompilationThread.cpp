d7a882725c9b84e3585cbbffb04d624b59c2b02b
Dhruv Chopra <Dhruv.C.Chopra@ibm.com>
2019-12-16 14:37:59 -0500

    Set JITServer Compilations Options on the client instead
    Previously, these options were being set as global options
    on the server side during startup. This is incorrect, as options
    set on the server are overwritten by the client options when the
    server receives a compilation request from a client.
    
    This commit now sets the options on the client, right before a remote
    compilation request is sent to the server.
    
    Signed-off-by: Dhruv Chopra <Dhruv.C.Chopra@ibm.com>

--- runtime/compiler/control/CompilationThread.cpp
+++ runtime/compiler/control/CompilationThread.cpp
@@ -7922,7 +7922,18 @@ TR::CompilationInfoPerThreadBase::wrappedCompile(J9PortLibrary *portLib, void * 
                   that->getCompThreadId());
             // JITServer TODO determine if we care to support annotations
             if (that->_methodBeingCompiled->isRemoteCompReq())
+               {
                options->setOption(TR_EnableAnnotations,false);
+
+               // This option is used to generate SIMD instructions on Z. Currently the infrastructure
+               // to support the relocation of some of those instructions is not available. Thus we disable
+               // this option for remote compilations.
+               options->setOption(TR_DisableSIMDArrayTranslate);
+
+               // Infrastructure to support the TOC is currently not available for Remote Compilations. We disable the feature
+               // here so that the codegen doesn't generate TOC enabled code as it won't be valid on the client JVM.
+               options->setOption(TR_DisableTOC);
+               }
             // Determine if known annotations exist and if so, keep annotations enabled
             if (!that->_methodBeingCompiled->isAotLoad() && !vm->isAOT_DEPRECATED_DO_NOT_USE() && options->getOption(TR_EnableAnnotations))
                {
