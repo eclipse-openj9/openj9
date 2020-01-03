4df859b99a86a0e820b4cf5d0e3d6b454016db53
Irwin D'Souza <dsouzai.gh@gmail.com>
2020-01-03 14:34:57 -0500

    Update copyrights
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- runtime/compiler/arm/codegen/ARMJNILinkage.cpp
+++ runtime/compiler/arm/codegen/ARMJNILinkage.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016, 2019 IBM Corp. and others
+ * Copyright (c) 2016, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
