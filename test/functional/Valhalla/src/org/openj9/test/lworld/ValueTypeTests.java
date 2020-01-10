c96ead6d5a44d9aba429d8b7a09701b5dd5aed9e
Tobi Ajila <atobia@ca.ibm.com>
2020-01-10 14:56:52 -0500

    Fix options for valhalla tests
    Some of the existing tests cannot pass with the noGC options. The noGC
    option is required while the GC work on scanning flattened arrays is
    still underway. noGC is not required when when flattening is disabled.
    The variation options are updated to reflect this.
    
    Signed-off-by: Tobi Ajila <atobia@ca.ibm.com>

--- test/functional/Valhalla/src/org/openj9/test/lworld/ValueTypeTests.java
+++ test/functional/Valhalla/src/org/openj9/test/lworld/ValueTypeTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2019 IBM Corp. and others
+ * Copyright (c) 2018, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -1381,7 +1381,7 @@ public class ValueTypeTests {
 	 * 	int y;
 	 * }
 	 */
-	@Test(priority=1)
+	@Test(enabled = false, priority=1)
 	static public void testCreateLargeNumberOfPoint2D() throws Throwable {
 		String fields[] = {"x:I", "y:I"};
 		String className = "Point2D";
