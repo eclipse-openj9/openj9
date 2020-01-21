482d77d2f003432fe7ed21d70ab324e529f59b94
Shubham Verma <shubhamv.sv@gmail.com>
2020-01-21 10:20:08 -0500

    Fix for zlinux JCL_Test_JITHelpers hangs on z15
    Compare and branch condition has been chagned to greater than or equal
    from greater than. Previously this resulted in the evaluator skipping
    the last index, hence the test failures. Moving the compare and branch
    instruction a line above s1VecStartIndex add instruction fixes those
    test failures.
    
    Added test to force branch to `labelS2PartialMatch` from within
    `labelMatchS2Loop`
    
    This fix is based on @fjeremic's suggestion in eclipse#8159
    
    Signed-off-by: Shubham Verma <shubhamv.sv@gmail.com>

--- test/functional/Java8andUp/src/org/openj9/test/com/ibm/jit/Test_JITHelpers.java
+++ test/functional/Java8andUp/src/org/openj9/test/com/ibm/jit/Test_JITHelpers.java
@@ -1,7 +1,7 @@
 package org.openj9.test.com.ibm.jit;
 
 /*******************************************************************************
- * Copyright (c) 1998, 2019 IBM Corp. and others
+ * Copyright (c) 1998, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -912,6 +912,7 @@ public class Test_JITHelpers {
 				Assert.assertEquals(helpers.intrinsicIndexOfStringLatin1(valueField.get("ab12345678901234567cdefghijklmnopq"), 34, valueField.get("de"), 2, 5), 20);
 				Assert.assertEquals(helpers.intrinsicIndexOfStringLatin1(valueField.get("ab12345678901234567cdefghijklmnopq"), 34, valueField.get("def"), 3, 5), 20);
 				Assert.assertEquals(helpers.intrinsicIndexOfStringLatin1(valueField.get("ab12345678901234567cdefghijklmnopq"), 34, valueField.get("defghijklmn"), 11, 5), 20);
+				Assert.assertEquals(helpers.intrinsicIndexOfStringLatin1(valueField.get("abcdefghiklmnopqrstwyzabcdefghikabcdefghiklmnopqlmmnopqrstwyzabcdepoisd"), 71, valueField.get("abcdefghiklmnopqlmmnopqrstwyzabcdepoisd"), 39, 0), 32);
 			}
 		} catch (IllegalAccessException e) {
 			throw new RuntimeException(e);
