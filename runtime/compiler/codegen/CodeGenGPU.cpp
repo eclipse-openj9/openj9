81eca2b4956faf3f7609ae0fdc9710f2b66fb510
Bohao(Aaron) Wang <aaronwang0407@gmail.com>
2020-03-03 10:01:43 -0500

    Remove references to deprecated ILOpcode iuneg
    Signed-off-by: Bohao(Aaron) Wang <aaronwang0407@gmail.com>

--- runtime/compiler/codegen/CodeGenGPU.cpp
+++ runtime/compiler/codegen/CodeGenGPU.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corp. and others
+ * Copyright (c) 2000, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -2634,8 +2634,8 @@ J9::CodeGenerator::printNVVMIR(
                    isLongMul ? "i64" : "i32");
       }
    else if (node->getOpCodeValue() == TR::bneg || node->getOpCodeValue() == TR::sneg ||
-            node->getOpCodeValue() == TR::ineg || node->getOpCodeValue() == TR::lneg ||
-            node->getOpCodeValue() == TR::iuneg || node->getOpCodeValue() == TR::luneg ||
+            node->getOpCodeValue() == TR::ineg || node->getOpCodeValue() == TR::lneg || 
+            node->getOpCodeValue() == TR::luneg ||
             node->getOpCodeValue() == TR::fneg || node->getOpCodeValue() == TR::dneg)
       {
       getNodeName(node->getChild(0), name0, self()->comp());
