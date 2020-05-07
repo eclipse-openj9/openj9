e7dede50c26dabe62114a42d128e1cc63767ae46
Filip Jeremic <fjeremic@ca.ibm.com>
2020-05-07 15:34:54 -0400

    Reduce TR_TraceTrees queries
    Some `TR_TraceTrees` trace option queries are OR'ed with other tracing,
    most notably codegen tracing which should be controlled via
    `TR_TraceCG`.
    
    Signed-off-by: Filip Jeremic <fjeremic@ca.ibm.com>

--- runtime/compiler/z/codegen/J9CodeGenPhase.cpp
+++ runtime/compiler/z/codegen/J9CodeGenPhase.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2019 IBM Corp. and others
+ * Copyright (c) 2000, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -52,7 +52,7 @@ J9::Z::CodeGenPhase::performReduceSynchronizedFieldLoadPhase(TR::CodeGenerator *
 
    if (obj.perform())
       {
-      if (cg->comp()->getOption(TR_TraceCG) || cg->comp()->getOption(TR_TraceTrees))
+      if (cg->comp()->getOption(TR_TraceCG))
          {
          cg->comp()->dumpMethodTrees("Post Reduce Synchronized Field Load Trees");
          }
