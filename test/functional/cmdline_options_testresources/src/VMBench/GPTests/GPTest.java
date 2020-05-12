4c4a874dcf23307ce1dc0003e311bf955095ccda
Babneet Singh <sbabneet@ca.ibm.com>
2020-05-11 22:40:24 -0400

    Add tests for -XX:[+|-]HandleSIGABRT
    - Copied functional/cmdLineTests/gptest to create sigabrtHandlingTest
    - class GPTest updated to output "Invoking abort!"
    - sigabrtHandlingTest.xml contains testcases for -XX:[+|-]HandleSIGABRT
    
    Signed-off-by: Babneet Singh <sbabneet@ca.ibm.com>

--- test/functional/cmdline_options_testresources/src/VMBench/GPTests/GPTest.java
+++ test/functional/cmdline_options_testresources/src/VMBench/GPTests/GPTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2019 IBM Corp. and others
+ * Copyright (c) 2001, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -104,6 +104,7 @@ public void run() {
 	}
 	
 	if (arg.equalsIgnoreCase("abort")) {
+		System.out.println("Invoking abort!");
 		this.gpAbort();
 		System.err.println("Survived abort!");
 		System.exit(3);
