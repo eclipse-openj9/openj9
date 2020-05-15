d1aa1053e497a68dd0fdcb66ccc90976e642b83b
Devin Nakamura <devinn@ca.ibm.com>
2020-05-15 13:30:06 -0400

    Fix warning in jvmti agent test
    Compiler warned of passing char* to function expecting unsigned char*.
    Same cast can be seen on other calls to Deallocate.
    
    Signed-off-by: Devin Nakamura <devinn@ca.ibm.com>

--- runtime/tests/jvmtitests/agent/error.c
+++ runtime/tests/jvmtitests/agent/error.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2001, 2019 IBM Corp. and others
+ * Copyright (c) 2001, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -60,7 +60,7 @@ throwJVMTIError(agentEnv * env, JNIEnv * jni_env, jvmtiError err)
 
 	if ((*jvmti_env)->GetErrorName(jvmti_env, err, &error) == JVMTI_ERROR_NONE) {
 		throwError(jni_env, error);
-		(*jvmti_env)->Deallocate(jvmti_env, error);
+		(*jvmti_env)->Deallocate(jvmti_env, (unsigned char *) error);
 	} else {
 		throwError(jni_env, "Failed to retrieve JVMTI error name");
 	}
