2285b28e41c9a30905b4415a844bae664987c58b
Harry Yu <harryyu1994@gmail.com>
2020-06-09 14:56:01 -0400

    Override cpu asserts in x86
    The intention is to disable the cpu asserts for JITServer
    
    Signed-off-by: Harry Yu <harryyu1994@gmail.com>

--- runtime/compiler/x/env/J9CPU.cpp
+++ runtime/compiler/x/env/J9CPU.cpp
@@ -129,5 +129,145 @@ J9::X86::CPU::getX86ProcessorFeatureFlags8()
    return self()->queryX86TargetCPUID()->_featureFlags8;
    }
 
+bool
+J9::X86::CPU::is_test(OMRProcessorArchitecture p)
+   {
+#if defined(J9VM_OPT_JITSERVER)
+   if (TR::CompilationInfo::getStream())
+      return true;
+#endif /* defined(J9VM_OPT_JITSERVER) */
 
+   switch(p)
+      {
+      case OMR_PROCESSOR_X86_INTELWESTMERE:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelWestmere() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELNEHALEM:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelNehalem() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELPENTIUM:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelPentium() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELP6:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelP6() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELPENTIUM4:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelPentium4() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELCORE2:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelCore2() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELTULSA:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelTulsa() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELSANDYBRIDGE:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelSandyBridge() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELIVYBRIDGE:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelIvyBridge() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELHASWELL:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelHaswell() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELBROADWELL:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelBroadwell() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_INTELSKYLAKE:
+         return TR::CodeGenerator::getX86ProcessorInfo().isIntelSkylake() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_AMDATHLONDURON:
+         return TR::CodeGenerator::getX86ProcessorInfo().isAMDAthlonDuron() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_AMDOPTERON:
+         return TR::CodeGenerator::getX86ProcessorInfo().isAMDOpteron() == (_processorDescription.processor == p);
+      case OMR_PROCESSOR_X86_AMDFAMILY15H:
+         return TR::CodeGenerator::getX86ProcessorInfo().isAMD15h() == (_processorDescription.processor == p);
+      default:
+         return false;
+      }
+   return false;
+   }
 
+bool
+J9::X86::CPU::supports_feature_test(uint32_t feature)
+   {
+#if defined(J9VM_OPT_JITSERVER)
+   if (TR::CompilationInfo::getStream())
+      return true;
+#endif /* defined(J9VM_OPT_JITSERVER) */
+
+   OMRPORT_ACCESS_FROM_OMRPORT(TR::Compiler->omrPortLib);
+   bool ans = (TRUE == omrsysinfo_processor_has_feature(&_processorDescription, feature));
+
+   switch(feature)
+      {
+      case OMR_FEATURE_X86_OSXSAVE:
+         return TR::CodeGenerator::getX86ProcessorInfo().enabledXSAVE() == ans;
+      case OMR_FEATURE_X86_FPU:
+         return TR::CodeGenerator::getX86ProcessorInfo().hasBuiltInFPU() == ans;
+      case OMR_FEATURE_X86_VME:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsVirtualModeExtension() == ans;
+      case OMR_FEATURE_X86_DE:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsDebuggingExtension() == ans;
+      case OMR_FEATURE_X86_PSE:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsPageSizeExtension() == ans;
+      case OMR_FEATURE_X86_TSC:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsRDTSCInstruction() == ans;
+      case OMR_FEATURE_X86_MSR:
+         return TR::CodeGenerator::getX86ProcessorInfo().hasModelSpecificRegisters() == ans;
+      case OMR_FEATURE_X86_PAE:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsPhysicalAddressExtension() == ans;
+      case OMR_FEATURE_X86_MCE:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsMachineCheckException() == ans;
+      case OMR_FEATURE_X86_CX8:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsCMPXCHG8BInstruction() == ans;
+      case OMR_FEATURE_X86_CMPXCHG16B:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsCMPXCHG16BInstruction() == ans;
+      case OMR_FEATURE_X86_APIC:
+         return TR::CodeGenerator::getX86ProcessorInfo().hasAPICHardware() == ans;
+      case OMR_FEATURE_X86_MTRR:
+         return TR::CodeGenerator::getX86ProcessorInfo().hasMemoryTypeRangeRegisters() == ans;
+      case OMR_FEATURE_X86_PGE:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsPageGlobalFlag() == ans;
+      case OMR_FEATURE_X86_MCA:
+         return TR::CodeGenerator::getX86ProcessorInfo().hasMachineCheckArchitecture() == ans;
+      case OMR_FEATURE_X86_CMOV:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsCMOVInstructions() == ans;
+      case OMR_FEATURE_X86_PAT:
+         return TR::CodeGenerator::getX86ProcessorInfo().hasPageAttributeTable() == ans;
+      case OMR_FEATURE_X86_PSE_36:
+         return TR::CodeGenerator::getX86ProcessorInfo().has36BitPageSizeExtension() == ans;
+      case OMR_FEATURE_X86_PSN:
+         return TR::CodeGenerator::getX86ProcessorInfo().hasProcessorSerialNumber() == ans;
+      case OMR_FEATURE_X86_CLFSH:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsCLFLUSHInstruction() == ans;
+      case OMR_FEATURE_X86_DS:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsDebugTraceStore() == ans;
+      case OMR_FEATURE_X86_ACPI:
+         return TR::CodeGenerator::getX86ProcessorInfo().hasACPIRegisters() == ans;
+      case OMR_FEATURE_X86_MMX:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsMMXInstructions() == ans;
+      case OMR_FEATURE_X86_FXSR:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsFastFPSavesRestores() == ans;
+      case OMR_FEATURE_X86_SSE:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsSSE() == ans;
+      case OMR_FEATURE_X86_SSE2:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsSSE2() == ans;
+      case OMR_FEATURE_X86_SSE3:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsSSE3() == ans;
+      case OMR_FEATURE_X86_SSSE3:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsSSSE3() == ans;
+      case OMR_FEATURE_X86_SSE4_1:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsSSE4_1() == ans;
+      case OMR_FEATURE_X86_SSE4_2:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsSSE4_2() == ans;
+      case OMR_FEATURE_X86_PCLMULQDQ:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsCLMUL() == ans;
+      case OMR_FEATURE_X86_AESNI:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsAESNI() == ans;
+      case OMR_FEATURE_X86_POPCNT:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsPOPCNT() == ans;
+      case OMR_FEATURE_X86_SS:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsSelfSnoop() == ans;
+      case OMR_FEATURE_X86_RTM:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsTM() == ans;
+      case OMR_FEATURE_X86_HTT:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsHyperThreading() == ans;
+      case OMR_FEATURE_X86_HLE:
+         return TR::CodeGenerator::getX86ProcessorInfo().supportsHLE() == ans;
+      case OMR_FEATURE_X86_TM:
+         return TR::CodeGenerator::getX86ProcessorInfo().hasThermalMonitor() == ans;
+      case OMR_FEATURE_X86_AVX:
+         return true;
+      default:
+         return false;
+      }
+   return false;
+   }
