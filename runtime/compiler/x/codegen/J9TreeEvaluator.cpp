1494dec11f775bd848db71ef72bc95a7bda2c01b
Liqun Liu <liqunl@ca.ibm.com>
2020-06-15 13:44:43 -0400

    Trap when DF flag is incorrectly set
    Check DF flag at method entry/exit, object/array new, arraycopy and
    calls in mainline. Trap if it is incorrectly set.
    
    The check is off by default, it can be turned on by setting
    environment variable `TR_enableBreakOnDFSet`.
    
    Incorrect DF flag is the cause of a memory corruption defect in which a
    rep stosb instruction is executed in the wrong direction. This defect
    is intermittent and hasn't been reproduced. This change will provide a
    way to catch such case in our builds.
    
    Signed-off-by: Liqun Liu <liqunl@ca.ibm.com>

--- runtime/compiler/x/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/x/codegen/J9TreeEvaluator.cpp
@@ -1253,6 +1253,12 @@ TR::Register *J9::X86::TreeEvaluator::newEvaluator(TR::Node *node, TR::CodeGener
       bool spillFPRegs = (comp->canAllocateInlineOnStack(node, classInfo) <= 0);
       targetRegister = TR::TreeEvaluator::performHelperCall(node, NULL, TR::acall, spillFPRegs, cg);
       }
+   else if (cg->canEmitBreakOnDFSet())
+      {
+      // Check DF flag after inline new
+      generateBreakOnDFSet(cg);
+      }
+
    return targetRegister;
    }
 
@@ -1523,6 +1529,9 @@ TR::Register *J9::X86::TreeEvaluator::multianewArrayEvaluator(TR::Node *node, TR
 
 TR::Register *J9::X86::TreeEvaluator::arraycopyEvaluator(TR::Node *node, TR::CodeGenerator *cg)
    {
+   if (cg->canEmitBreakOnDFSet())
+      generateBreakOnDFSet(cg);
+
    TR::Compilation *comp = cg->comp();
 
    if (!node->isReferenceArrayCopy())
@@ -11500,7 +11509,6 @@ J9::X86::TreeEvaluator::tabortEvaluator(TR::Node *node, TR::CodeGenerator *cg)
 TR::Register *
 J9::X86::TreeEvaluator::directCallEvaluator(TR::Node *node, TR::CodeGenerator *cg)
    {
-
    static bool useJapaneseCompression = (feGetEnv("TR_JapaneseComp") != NULL);
    TR::Compilation *comp = cg->comp();
    TR::SymbolReference *symRef = node->getSymbolReference();
