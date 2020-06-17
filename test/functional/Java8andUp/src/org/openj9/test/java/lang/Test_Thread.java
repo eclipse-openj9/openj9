ff1fd6906e3f268067cff60e2aae971d59ada6f4
Peter Shipton <Peter_Shipton@ca.ibm.com>
2020-06-17 14:46:01 -0400

    Improve Test_Thread.test_join3() failure diagnostics
    [ci skip]
    
    Signed-off-by: Peter Shipton <Peter_Shipton@ca.ibm.com>

--- test/functional/Java8andUp/src/org/openj9/test/java/lang/Test_Thread.java
+++ test/functional/Java8andUp/src/org/openj9/test/java/lang/Test_Thread.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1998, 2019 IBM Corp. and others
+ * Copyright (c) 1998, 2020 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -883,6 +883,7 @@ public class Test_Thread {
 			AssertJUnit.assertTrue("Joined thread is not alive", t.isAlive());
 			t.interrupt();
 		} catch (Exception e) {
+			e.printStackTrace();
 			AssertJUnit.assertTrue("Exception during test: " + e.toString(), false);
 		}
 
