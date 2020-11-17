d9a963033c8d9591df5d8eefec5325e84e204ebb
Peter Shipton <Peter_Shipton@ca.ibm.com>
2020-11-17 12:22:59 -0500

    Include JAVA_VENDOR, J9PRODUCT_NAME, J9JDK_EXT_VERSION in javacore
    These defines are set by openj9_version_info.h from the extensions.
    
    Signed-off-by: Peter Shipton <Peter_Shipton@ca.ibm.com>

--- runtime/include/vendor_version.h
+++ runtime/include/vendor_version.h
@@ -20,10 +20,10 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
-/* This file provides a means to supply vendor specific version info such as 
- * short name, SHA, and version string.
+/* This file provides a means to supply vendor specific version info such as
+ * vendor name, short name, SHA, and version string.
  * These vendor version info can be defined either in this file or other places.
- * 
+ *
  * Example usage for inclusion of a vendor name and repository sha.  These values
  * will be inserted into the java.fullversion and java.vm.info system properties
  * and in a generated javacore file.
@@ -51,15 +51,5 @@
 
 #define JAVA_VM_VENDOR "Eclipse OpenJ9"
 #define JAVA_VM_NAME "Eclipse OpenJ9 VM"
-
-#if JAVA_SPEC_VERSION < 12
-/* Pre-JDK12 versions use following defines to set system properties
- * java.vendor and java.vendor.url within vmprop.c:initializeSystemProperties(vm).
- * JDK12 (assuming future versions as well) sets these properties via java.lang.VersionProps.init(systemProperties) 
- * and following settings within System.ensureProperties().
- */
-#define JAVA_VENDOR "Eclipse OpenJ9"
-#define JAVA_VENDOR_URL "http://www.eclipse.org/openj9"
-#endif /* JAVA_SPEC_VERSION < 12 */
 
 #endif     /* vendor_version_h */
