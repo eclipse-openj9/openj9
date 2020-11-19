afc71a5f8f3c9b71ee5ef40282d2e9043efc2a38
Babneet Singh <sbabneet@ca.ibm.com>
2020-11-19 12:10:33 -0500

    Add stubs for the ScopedMemoryAccess native methods [JDK16]
    Native methods:
    1. private static native void registerNatives();
    2. native boolean closeScope0(Scope scope, Scope.ScopedAccessError
    exception);
    
    Signed-off-by: Babneet Singh <sbabneet@ca.ibm.com>

--- runtime/jcl/exports.cmake
+++ runtime/jcl/exports.cmake
@@ -591,5 +591,7 @@ endif()
 if(NOT JAVA_SPEC_VERSION LESS 16)
 	omr_add_exports(jclse
 		Java_java_lang_ref_Reference_refersTo
+		Java_jdk_internal_misc_ScopedMemoryAccess_registerNatives
+		Java_jdk_internal_misc_ScopedMemoryAccess_closeScope0
 	)
 endif()
