64d08d4e98fd137fdee17fada37068807af473c7
Keith W. Campbell <keithc@ca.ibm.com>
2021-01-14 10:34:55 -0500

    Fix typos, formatting
    * add missing @Overrides annotations
    * minimize scope of @SuppressWarnings annotations
    * use StandardCharsets
    
    Signed-off-by: Keith W. Campbell <keithc@ca.ibm.com>

--- debugtools/DDR_VM/src/com/ibm/j9ddr/vm29/j9/DataType.java
+++ debugtools/DDR_VM/src/com/ibm/j9ddr/vm29/j9/DataType.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1991, 2019 IBM Corp. and others
+ * Copyright (c) 1991, 2021 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -19,7 +19,6 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
- 
 package com.ibm.j9ddr.vm29.j9;
 
 import com.ibm.j9ddr.CorruptDataException;
@@ -33,7 +32,7 @@ import com.ibm.j9ddr.vm29.pointer.generated.J9RASPointer;
 import com.ibm.j9ddr.vm29.types.UDATA;
 
 public abstract class DataType {
-	
+
 	// Fields
 	protected static IProcess process;
 	protected static byte SIZEOF_BOOL;
@@ -43,7 +42,7 @@ public abstract class DataType {
 	protected static String structurePackageName;
 
 	// Static methods
-	
+
 	// Invoked via reflection from VMDataFactory.getVMData(IProcess);
 	public static void init(IProcess process, StructureReader structureReader) {
 		DataType.process = process;
@@ -52,24 +51,23 @@ public abstract class DataType {
 		pointerPackageName = structureReader.getPackageName(PackageNameType.POINTER_PACKAGE_DOT_NAME);
 		structurePackageName = structureReader.getPackageName(PackageNameType.STRUCTURE_PACKAGE_DOT_NAME);
 	}
-	
+
 	// Called via reflection in VMDataFactory.getVMDaata(IProcess)
-	public static void setJ9RASPointer(J9RASPointer j9RASPoitner) {
-		J9RASPOINTER = j9RASPoitner;
+	public static void setJ9RASPointer(J9RASPointer j9RASPointer) {
+		J9RASPOINTER = j9RASPointer;
 	}
-	
+
 	public static J9RASPointer getJ9RASPointer() {
 		return J9RASPOINTER;
 	}
-	
+
 	public static IProcess getProcess() {
 		return process;
 	}
-			
+
 	@SuppressWarnings("unchecked")
-	public static DataType getStructure(String name, long address) 
-	{	
-		if(address == 0) {
+	public static DataType getStructure(String name, long address) {
+		if (address == 0) {
 			return null;
 		}
 
@@ -78,53 +76,50 @@ public abstract class DataType {
 			try {
 				// Probably a structure pointer
 				clazz = (Class<? extends StructurePointer>) Class.forName(pointerPackageName + "." + name);
-			} catch(ClassNotFoundException notFound) {
+			} catch (ClassNotFoundException notFound) {
 				// Might be a built-in type
 				try {
-					clazz = (Class<? extends DataType>)Class.forName(UDATAPointer.class.getPackage().getName() + "." + name);
-				} catch(ClassNotFoundException stillNotFound) {
+					clazz = (Class<? extends DataType>) Class.forName(UDATAPointer.class.getPackage().getName() + "." + name);
+				} catch (ClassNotFoundException stillNotFound) {
 					throw notFound;
 				}
-			}	
+			}
 			return getStructure(clazz, address);
 		} catch (Exception e) {
 			throw new IllegalStateException("Cannot create the structure " + name, e);
 		}
 	}
 
-	public static DataType getStructure(String name, UDATA udata) 
-	{
+	public static DataType getStructure(String name, UDATA udata) {
 		return getStructure(name, udata.longValue());
 	}
-	
-	@SuppressWarnings("unchecked")
-	public static <T> T getStructure(Class<T> clazz, long address) 
-	{
-		if(address == 0) {
+
+	public static <T> T getStructure(Class<T> clazz, long address) {
+		if (address == 0) {
 			return null;
 		}
-		
+
 		try {
 			java.lang.reflect.Method method = clazz.getDeclaredMethod("cast", new Class[] { long.class });
-			T result = (T) method.invoke(null, new Object[] { address});
+			@SuppressWarnings("unchecked")
+			T result = (T) method.invoke(null, new Object[] { address });
 			return result;
 		} catch (Exception e) {
 			throw new IllegalStateException("Cannot create the structure " + clazz.getSimpleName(), e);
 		}
 	}
-	
+
 	// Instance methods
-	
+
 	// Answer the long representation of the receiver.  This value may not have useful meaning in all contexts
 	public abstract long longValue() throws CorruptDataException;
-	
+
 	/**
 	 * @return Formats this type for DDR interactive, short version. E.g. u8: 0xFF (255)
 	 */
-	public String formatShortInteractive()
-	{
+	public String formatShortInteractive() {
 		StringBuilder builder = new StringBuilder();
-		
+
 		builder.append(this.getClass().getSimpleName().toLowerCase());
 		builder.append(": 0x");
 		try {
@@ -134,7 +129,7 @@ public abstract class DataType {
 		} catch (InvalidDataTypeException e) {
 			builder.append("<FAULT : " + e.getMessage() + ">");
 		}
-		
+
 		builder.append(" (");
 		try {
 			builder.append(this.longValue());
@@ -142,17 +137,16 @@ public abstract class DataType {
 			builder.append("FAULT");
 		}
 		builder.append(")");
-		
+
 		return builder.toString();
 	}
-	
-	public static String getPointerPackageName() 
-	{
+
+	public static String getPointerPackageName() {
 		return pointerPackageName;
 	}
-	
-	public static String getStructurePackageName() 
-	{
+
+	public static String getStructurePackageName() {
 		return structurePackageName;
 	}
+
 }
