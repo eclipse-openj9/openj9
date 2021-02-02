2abc4403444abf37057f92e0df7eba3bfabefaaa
Oussama Saoudi <oussama.saoudi@ibm.com>
2021-02-02 13:16:17 -0800

    Change doClassSlot doVMClassSlot to use address of class instead of slot
    Changed doClassSlot and doVMClassSlot methods to take class pointer
    instead of the class slot pointer for the following classes:
    - RootScanner
    - ReferenceChainWalker
    - HeapRootScanner
    
    Signed-off-by: Oussama Saoudi <oussama.saoudi@ibm.com>

--- runtime/gc_realtime/RealtimeRootScanner.hpp
+++ runtime/gc_realtime/RealtimeRootScanner.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 1991, 2020 IBM Corp. and others
+ * Copyright (c) 1991, 2021 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -96,7 +96,7 @@ public:
 
 	virtual const char* scannerName() = 0;
 	
-	virtual void doClassSlot(J9Class **clazzPtr);
+	virtual void doClassSlot(J9Class *classPtr);
 
 	virtual void scanMonitorLookupCaches(MM_EnvironmentBase *env);
 
