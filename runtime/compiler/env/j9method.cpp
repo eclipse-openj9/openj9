2c98de641651bb9dab2989de08692094e05e23a4
Irwin D'Souza <dsouzai.gh@gmail.com>
2021-02-04 16:53:28 -0500

    Add definition for snprintf for MSC_VER < 1900
    Signed-off-by: Irwin D'Souza <dsouzai.gh@gmail.com>

--- runtime/compiler/env/j9method.cpp
+++ runtime/compiler/env/j9method.cpp
@@ -20,6 +20,10 @@
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
  *******************************************************************************/
 
+#if defined (_MSC_VER) && (_MSC_VER < 1900)
+#define snprintf _snprintf
+#endif
+
 #include "env/j9method.h"
 
 #include <stddef.h>
