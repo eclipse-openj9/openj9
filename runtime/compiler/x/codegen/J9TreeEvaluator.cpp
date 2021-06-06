ba4f2d649c2b95f48a33f95bf41ee4520aa01f99
Josh Soref <jsoref@gmail.com>
2021-06-06 06:17:56 -0400

    spelling: argument

--- runtime/compiler/x/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/x/codegen/J9TreeEvaluator.cpp
@@ -12406,7 +12406,7 @@ void generateReportFieldAccessOutlinedInstructions(TR::Node *node, TR::LabelSymb
 
    /*
     * For reporting field write, reference to the valueNode (valueNode is evaluated in valueReg) is needed so we need to store
-    * the value on to a stack location first and pass the stack location address as an arguement
+    * the value on to a stack location first and pass the stack location address as an argument
     * to the VM helper
     */
    if (isWrite)
