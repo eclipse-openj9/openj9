ff62a7a2ac1b21ceab9a89963c0a62ad10a0b36f
Daryl Maier <maier@ca.ibm.com>
2021-08-20 15:48:52 -0400

    Prevent inlining of LambdaForm generated reinvoke() methods
    LambdaForm generated reinvoke() methods appear on slow paths.  However,
    inlining into them and their descendents consumes inlining budget.
    Manually exclude them from inlining consideration.
    
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- runtime/compiler/optimizer/InlinerTempForJ9.cpp
+++ runtime/compiler/optimizer/InlinerTempForJ9.cpp
@@ -4999,6 +4999,23 @@ TR_InlinerFailureReason
          break;
    }
 
+   /**
+    * Do not inline LambdaForm generated reinvoke() methods as they are on the
+    * slow path and may consume inlining budget.
+    */
+   if (comp->fej9()->isLambdaFormGeneratedMethod(resolvedMethod))
+      {
+      if (resolvedMethod->nameLength() == strlen("reinvoke") &&
+          !strncmp(resolvedMethod->nameChars(), "reinvoke", strlen("reinvoke")))
+         {
+         traceMsg(comp, "Intentionally avoided inlining generated %.*s.%.*s%.*s\n",
+                  resolvedMethod->classNameLength(), resolvedMethod->classNameChars(),
+                  resolvedMethod->nameLength(), resolvedMethod->nameChars(),
+                  resolvedMethod->signatureLength(), resolvedMethod->signatureChars());
+         return DontInline_Callee;
+         }
+      }
+
    if (comp->getOptions()->getEnableGPU(TR_EnableGPU))
       {
       switch (rm)
