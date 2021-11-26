9df80dcb4cdb4eaa111cbe48a15fc16bce931a39
Jason Feng <fengj@ca.ibm.com>
2021-11-25 22:45:21 -0500

    Adopt RI java.lang.Thread/ThreadGroup w/ opt_ojdkThreadSupport
    Added opt_ojdkThreadSupport flag and decorate the behaviour change w/
    J9VM_OPT_OJDK_THREAD_SUPPORT;
    Replaced j.l.Thread.threadRef with eetop;
    Replaced j.l.Thread.isDaemon with daemon;
    Replaced j.l.Thread.runnable with target;
    Replaced j.l.Thread.lock with blockerLock;
    Replaced j.l.ThreadGroup.numThreads with nthreads;
    Replaced j.l.ThreadGroup.numGroups with ngroups;
    Replaced j.l.ThreadGroup.childrenThreads with threads;
    Replaced j.l.ThreadGroup.childrenGroups with groups;
    Replaced j.l.ThreadGroup.isDaemon with daemon;
    Updated references accordingly;
    Minor refactoring at methods affected.
    
    
    Signed-off-by: Jason Feng <fengj@ca.ibm.com>

--- runtime/cmake/options.cmake
+++ runtime/cmake/options.cmake
@@ -1,5 +1,5 @@
 ################################################################################
-# Copyright (c) 2017, 2021 IBM Corp. and others
+# Copyright (c) 2017, 2022 IBM Corp. and others
 #
 # This program and the accompanying materials are made available under
 # the terms of the Eclipse Public License 2.0 which accompanies this
@@ -138,6 +138,7 @@ option(J9VM_OPT_METHOD_HANDLE "Enables support for OpenJ9 MethodHandles. J9VM_OP
 option(J9VM_OPT_METHOD_HANDLE_COMMON "Enables common dependencies between OpenJ9 and OpenJDK MethodHandles.")
 option(J9VM_OPT_MODULE "Turns on module support")
 option(J9VM_OPT_MULTI_VM "Decides if multiple VMs can be created in the same address space")
+option(J9VM_OPT_OJDK_THREAD_SUPPORT "Enables OpenJDK Thread support for Project Loom.")
 option(J9VM_OPT_OPENJDK_METHODHANDLE "Enables support for OpenJDK MethodHandles. J9VM_OPT_METHOD_HANDLE should be disabled.")
 
 option(J9VM_OPT_PANAMA "Enables support for Project Panama features such as native method handles")
