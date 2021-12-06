71981a81f62951211cba8efb17489a70050df03f
Ehren Julien-Neitzert <ehren.julien-neitzert@ibm.com>
2021-12-06 10:45:22 -0800

    add tests for valueHeaderSize and uninitializedDefaultValue Unsafe apis
    Signed-off-by: Ehren Julien-Neitzert <ehren.julien-neitzert@ibm.com>

--- test/functional/Valhalla/src/org/openj9/test/lworld/ValueTypeUnsafeTests.java
+++ test/functional/Valhalla/src/org/openj9/test/lworld/ValueTypeUnsafeTests.java
@@ -24,6 +24,9 @@ package org.openj9.test.lworld;
 
 import jdk.internal.misc.Unsafe;
 import java.lang.reflect.Field;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.util.List;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
@@ -33,17 +36,25 @@ import static org.openj9.test.lworld.ValueTypeUnsafeTestClasses.*;
 @Test(groups = { "level.sanity" })
 public class ValueTypeUnsafeTests {
 	static Unsafe myUnsafe = null;
+	static boolean isCompressedRefsEnabled = false;
+	static boolean isFlatteningEnabled = false;
+	static boolean isArrayFlatteningEnabled = false;
 
 	@BeforeClass
 	static public void testSetUp() throws RuntimeException {
 		myUnsafe = Unsafe.getUnsafe();
+
+		List<String> arguments = ManagementFactory.getRuntimeMXBean().getInputArguments();
+		isFlatteningEnabled = arguments.contains("-XX:ValueTypeFlatteningThreshold=99999");
+		isArrayFlatteningEnabled = arguments.contains("-XX:+EnableArrayFlattening");
+		isCompressedRefsEnabled = arguments.contains("-Xcompressedrefs");
 	}
 
 	@Test
 	static public void testFlattenedFieldIsFlattened() throws Throwable {
 		ValueTypePoint2D p = new ValueTypePoint2D(new ValueTypeInt(1), new ValueTypeInt(1));
 		boolean isFlattened = myUnsafe.isFlattened(p.getClass().getDeclaredField("x"));
-		assertTrue(isFlattened);
+		assertEquals(isFlattened, isFlatteningEnabled);
 	}
 
 	@Test
@@ -51,7 +62,7 @@ public class ValueTypeUnsafeTests {
 		ValueTypePoint2D[] pointAry = {};
 		assertTrue(pointAry.getClass().isArray());
 		boolean isArrayFlattened = myUnsafe.isFlattenedArray(pointAry.getClass());
-		assertTrue(isArrayFlattened);
+		assertEquals(isArrayFlattened, isArrayFlatteningEnabled);
 	}
 
 	@Test
@@ -87,5 +98,96 @@ public class ValueTypeUnsafeTests {
 		assertThrows(NullPointerException.class, () -> {
 			myUnsafe.isFlattened(null);
 		});
+	}
+
+	@Test
+	static public void testValueHeaderSizeOfValueType() throws Throwable {
+		long headerSize = myUnsafe.valueHeaderSize(ValueTypePoint2D.class);
+		if (isCompressedRefsEnabled) {
+			assertEquals(headerSize, 4);
+		} else {
+			assertEquals(headerSize, 8);
+		}
+	}
+
+	@Test
+	static public void testValueHeaderSizeOfClassWithLongField() throws Throwable {
+		long headerSize = myUnsafe.valueHeaderSize(ValueTypeWithLongField.class);
+		if (isCompressedRefsEnabled && !isFlatteningEnabled) {
+			assertEquals(headerSize, 4);
+		} else {
+			// In compressed refs mode, when the class has 8 byte field(s) only, 4 bytes of padding are added to the class. These padding bytes are included in the headerSize.
+			assertEquals(headerSize, 8);
+		}
+	}
+
+	// TODO (#14117): add testing for Unsafe.valueHeaderSize using Unsafe.getObjectSize
+
+	@Test
+	static public void testValueHeaderSizeOfNonValueType() throws Throwable {
+		assertEquals(myUnsafe.valueHeaderSize(Point2D.class), 0);
+	}
+
+	@Test
+	static public void testValueHeaderSizeOfNull() throws Throwable {
+		assertEquals(myUnsafe.valueHeaderSize(null), 0);
+	}
+
+	@Test
+	static public void testValueHeaderSizeOfVTArray() throws Throwable {
+		ValueTypeInt[] vtAry = {};
+		long size = myUnsafe.valueHeaderSize(vtAry.getClass());
+		assertEquals(size, 0);
+	}
+
+	@Test
+	static public void testValueHeaderSizeOfArray() throws Throwable {
+		Point2D[] ary = {};
+		long size = myUnsafe.valueHeaderSize(ary.getClass());
+		assertEquals(size, 0);
+	}
+
+	@Test
+	static public void testUninitializedDefaultValueOfValueType() throws Throwable {
+		// create a new ValueTypePoint2D to ensure that the class has been initialized befoere myUnsafe.uninitializedDefaultValue is called
+		ValueTypePoint2D newPoint = new ValueTypePoint2D(new ValueTypeInt(1), new ValueTypeInt(1));
+		ValueTypePoint2D p = myUnsafe.uninitializedDefaultValue(newPoint.getClass());
+		assertEquals(p.x.i, 0);
+		assertEquals(p.y.i, 0);
+	}
+
+	@Test
+	static public void testUninitializedDefaultValueOfNonValueType() throws Throwable {
+		Point2D p = myUnsafe.uninitializedDefaultValue(Point2D.class);
+		assertNull(p);
+	}
+
+	@Test
+	static public void testUninitializedDefaultValueOfNull() throws Throwable {
+		Object o = myUnsafe.uninitializedDefaultValue(null);
+		assertNull(o);
+	}
+
+	@Test
+	static public void testUninitializedDefaultValueOfVTArray() throws Throwable {
+		ValueTypeInt[] vtAry = {};
+		Object o = myUnsafe.uninitializedDefaultValue(vtAry.getClass());
+		assertNull(o);
+	}
+
+	@Test
+	static public void testUninitializedDefaultValueOfArray() throws Throwable {
+		Point2D[] ary = {};
+		Object o = myUnsafe.uninitializedDefaultValue(ary.getClass());
+		assertNull(o);
+	}
+
+	@Test
+	static public void testuninitializedVTClassHasNullDefaultValue() {
+		primitive class NeverInitialized {
+			final ValueTypeInt i;
+			NeverInitialized(ValueTypeInt i) { this.i = i; }
+		}
+		assertNull(myUnsafe.uninitializedDefaultValue(NeverInitialized.class));
 	}
 }
