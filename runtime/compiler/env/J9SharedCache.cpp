8dbf950e6801f8b9257bdd4b0a4ddf2c69a59012
Christian Despres <despresc@ibm.com>
2022-01-06 15:51:26 -0500

    Remove chain exists messages from JITServer verbose log
    Fixes: #9961
    
    Signed-off-by: Christian Despres <despresc@ibm.com>

--- runtime/compiler/env/J9SharedCache.cpp
+++ runtime/compiler/env/J9SharedCache.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2021 IBM Corp. and others
+ * Copyright (c) 2000, 2022 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -887,7 +887,6 @@ TR_J9SharedCache::rememberClass(J9Class *clazz, const AOTCacheClassChainRecord *
    chainData = findChainForClass(clazz, key, keyLength);
    if (chainData != NULL)
       {
-      LOG(1, "\tchain exists (%p) so nothing to store\n", chainData);
       return chainData;
       }
 
