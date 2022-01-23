e8aa2decb0d6707ef24ba96f1096f3a94e7cace3
Alexey Khrabrov <khrabrov@cs.toronto.edu>
2022-01-23 17:34:07 -0500

    Remove TR_ResolvedJ9JITServerMethod::fieldOrStaticName()
    Since ROMClass serialization now includes all strings in the
    ROMClass, the implementation of this method is identical to
    the one in TR_ResolvedJ9MethodBase and can be removed.
    
    Signed-off-by: Alexey Khrabrov <khrabrov@cs.toronto.edu>

--- runtime/compiler/env/j9methodServer.hpp
+++ runtime/compiler/env/j9methodServer.hpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018, 2021 IBM Corp. and others
+ * Copyright (c) 2018, 2022 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -256,8 +256,8 @@ private:
    bool _isLambdaFormGeneratedMethod;
    bool _isForceInline;
 
-   virtual char * fieldOrStaticName(I_32 cpIndex, int32_t & len, TR_Memory * trMemory, TR_AllocationKind kind = heapAlloc) override;
-   void unpackMethodInfo(TR_OpaqueMethodBlock * aMethod, TR_FrontEnd * fe, TR_Memory * trMemory, uint32_t vTableSlot, TR::CompilationInfoPerThread *threadCompInfo, const TR_ResolvedJ9JITServerMethodInfo &methodInfo);
+   void unpackMethodInfo(TR_OpaqueMethodBlock *aMethod, TR_FrontEnd *fe, TR_Memory *trMemory, uint32_t vTableSlot,
+                         TR::CompilationInfoPerThread *threadCompInfo, const TR_ResolvedJ9JITServerMethodInfo &methodInfo);
    };
 
 
