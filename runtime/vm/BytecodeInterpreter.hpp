fd8f64268802d3165d60daffcdb296edd89a79f8
KONNO Kazuhiro <konno@jp.ibm.com>
2022-02-25 16:07:56 +0900

    (0.31.0) AArch64 macOS: Stop extending boolean to uint32 in JNI calls
    This commit partially reverts the change introduced by #4569, stopping
    extension of boolean to uint32 in JNI calls on AArch64 macOS.
    It also disables related testcases.
    
    Original PR in master: #14575 + #14607
    
    Signed-off-by: KONNO Kazuhiro <konno@jp.ibm.com>

--- runtime/vm/BytecodeInterpreter.hpp
+++ runtime/vm/BytecodeInterpreter.hpp
@@ -2312,7 +2312,16 @@ done:
 	getFFIType(U_8 j9ntc) {
 		static const ffi_type * const J9NtcToFFI[] = {
 				&ffi_type_void,		/* J9NtcVoid */
+#if defined(OSX) && defined(J9AARCH64)
+				/*
+				 * No extension from boolean to uint32 on AArch64 macOS
+				 * because its native ABI aligns arguments smaller than
+				 * 8 bytes at their natural boundaries on the stack
+				 */
+				&ffi_type_uint8,	/* J9NtcBoolean */
+#else
 				&ffi_type_uint32,	/* J9NtcBoolean */
+#endif /* defined(OSX) && defined(J9AARCH64) */
 				&ffi_type_sint8,	/* J9NtcByte */
 				&ffi_type_uint16,	/* J9NtcChar */
 				&ffi_type_sint16,	/* J9NtcShort */
