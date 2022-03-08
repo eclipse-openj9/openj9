9355aae7331dc7609310bc2b352dc6a3db0dd648
Jason Feng <fengj@ca.ibm.com>
2022-03-08 11:58:15 -0500

    Move CRIU.isCRIUSupportEnabled() & related APIs into java.base
    This makes it straightforward to check isCRIUSupportEnabled() from
    java.base (particularly for security related JCL patches).
    isCheckpointAllowed() invokes isCRIUSupportEnabled() first to load
    library j9criu29 and complete initialization.
    
    Signed-off-by: Jason Feng <fengj@ca.ibm.com>

--- jcl/src/openj9.criu/share/classes/org/eclipse/openj9/criu/CRIUSupport.java
+++ jcl/src/openj9.criu/share/classes/org/eclipse/openj9/criu/CRIUSupport.java
@@ -23,9 +23,7 @@
 package org.eclipse.openj9.criu;
 
 import java.nio.file.Path;
-/*[IF JAVA_SPEC_VERSION < 17] */
 import java.security.AccessController;
-/*[ENDIF] JAVA_SPEC_VERSION < 17 */
 import java.security.PrivilegedAction;
 import java.nio.file.Files;
 import java.util.ArrayList;
@@ -45,25 +43,19 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+import openj9.internal.criu.InternalCRIUSupport;
+
 /**
  * CRIU Support API
  */
+/*[IF JAVA_SPEC_VERSION >= 17]*/
+@SuppressWarnings({ "deprecation", "removal" })
+/*[ENDIF] JAVA_SPEC_VERSION >= 17 */
 public final class CRIUSupport {
 
 	@SuppressWarnings("restriction")
 	private static Unsafe unsafe;
-
 	private static final CRIUDumpPermission CRIU_DUMP_PERMISSION = new CRIUDumpPermission();
-
-	private static boolean criuSupportEnabled = false;
-
-	private static native boolean isCRIUSupportEnabledImpl();
-
-	private static native boolean isCheckpointAllowed();
-
-	private static boolean nativeLoaded = false;
-
-	private static boolean initComplete = false;
 
 	private static native void checkpointJVMImpl(String imageDir,
 			boolean leaveRunning,
@@ -76,6 +68,20 @@ public final class CRIUSupport {
 			boolean tcpEstablished,
 			boolean autoDedup,
 			boolean trackMemory);
+
+	static {
+		AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
+			try {
+				Field f = Unsafe.class.getDeclaredField("theUnsafe"); //$NON-NLS-1$
+				f.setAccessible(true);
+				unsafe = (Unsafe) f.get(null);
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException
+					| IllegalAccessException e) {
+				throw new InternalError(e);
+			}
+			return null;
+		});
+	}
 
 	/**
 	 * Constructs a new {@code CRIUSupport}.
@@ -107,9 +113,6 @@ public final class CRIUSupport {
 	 * @throws IllegalArgumentException if imageDir is not a valid directory
 	 */
 	public CRIUSupport(Path imageDir) {
-		/* [IF JAVA_SPEC_VERSION < 17] */
-		@SuppressWarnings({"deprecation" })
-		/* [ENDIF] JAVA_SPEC_VERSION < 17 */
 		SecurityManager manager = System.getSecurityManager();
 		if (manager != null) {
 			manager.checkPermission(CRIU_DUMP_PERMISSION);
@@ -118,39 +121,13 @@ public final class CRIUSupport {
 		setImageDir(imageDir);
 	}
 
-	@SuppressWarnings({ "restriction", "deprecation" })
-	private static synchronized void init() {
-		if (!initComplete) {
-			if (loadNativeLibrary()) {
-				criuSupportEnabled = isCRIUSupportEnabledImpl();
-				AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
-					try {
-						Field f = Unsafe.class.getDeclaredField("theUnsafe"); //$NON-NLS-1$
-						f.setAccessible(true);
-						unsafe = (Unsafe) f.get(null);
-					} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-						throw new InternalError(e);
-					}
-					return null;
-				});
-
-			}
-
-			initComplete = true;
-		}
-	}
-
 	/**
 	 * Queries if CRIU support is enabled.
 	 *
-	 * @return TRUE is support is enabled, FALSE otherwise
+	 * @return TRUE if support is enabled, FALSE otherwise
 	 */
-	public synchronized static boolean isCRIUSupportEnabled() {
-		if (!initComplete) {
-			init();
-		}
-
-		return criuSupportEnabled;
+	public static boolean isCRIUSupportEnabled() {
+		return InternalCRIUSupport.isCRIUSupportEnabled();
 	}
 
 	/**
@@ -160,18 +137,9 @@ public final class CRIUSupport {
 	 * @return NULL if isCRIUSupportEnabled() returns true. Otherwise the error message
 	 */
 	public static String getErrorMessage() {
-		String s = null;
-		if (!isCRIUSupportEnabled()) {
-			if (nativeLoaded) {
-				s = "To enable criu support, please run java with the `-XX:+EnableCRIUSupport` option.";
-			} else {
-				s = "There was a problem loaded the criu native library.\n"
-						+ "Please check that criu is installed on the machine by running `criu check`.\n"
-						+ "Also, please ensure that the JDK is criu enabled by contacting your JDK provider.";
-			}
-		}
-		return s;
+		return InternalCRIUSupport.getErrorMessage();
 	}
+
 	/* Higher priority hooks are run last in pre-checkoint hooks, and are run
 	 * first in post restore hooks.
 	 */
@@ -191,25 +159,6 @@ public final class CRIUSupport {
 	private boolean trackMemory;
 	private Path envFile;
 
-	@SuppressWarnings("deprecation")
-	private synchronized static boolean loadNativeLibrary() {
-		if (!nativeLoaded) {
-			try {
-				AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
-					System.loadLibrary("j9criu29"); //$NON-NLS-1$
-					nativeLoaded = true;
-					return null;
-				});
-			} catch (UnsatisfiedLinkError e) {
-				if (System.getProperty("enable.j9internal.checkpoint.hook.api.debug") != null) { //$NON-NLS-1$
-					e.printStackTrace();
-				}
-			}
-		}
-
-		return nativeLoaded;
-	}
-
 	/**
 	 * Sets the directory that will hold the images upon checkpoint. This must be
 	 * set before calling {@link #checkpointJVM()}.
@@ -227,9 +176,6 @@ public final class CRIUSupport {
 		}
 		String dir = imageDir.toAbsolutePath().toString();
 
-		/* [IF JAVA_SPEC_VERSION < 17] */
-		@SuppressWarnings({"deprecation" })
-		/* [ENDIF] JAVA_SPEC_VERSION < 17 */
 		SecurityManager manager = System.getSecurityManager();
 		if (manager != null) {
 			manager.checkWrite(dir);
@@ -391,9 +337,6 @@ public final class CRIUSupport {
 		}
 		String dir = workDir.toAbsolutePath().toString();
 
-		/* [IF JAVA_SPEC_VERSION < 17] */
-		@SuppressWarnings({"deprecation" })
-		/* [ENDIF] JAVA_SPEC_VERSION < 17 */
 		SecurityManager manager = System.getSecurityManager();
 		if (manager != null) {
 			manager.checkWrite(dir);
@@ -564,6 +507,8 @@ public final class CRIUSupport {
 	 * options setters.
 	 *
 	 * @throws UnsupportedOperationException if CRIU is not supported
+	 *  or running in non-portable mode (only one checkpoint is allowed),
+	 *  and we have already checkpointed once.
 	 * @throws JVMCheckpointException        if a JVM error occurred before
 	 *                                       checkpoint
 	 * @throws SystemCheckpointException     if a CRIU operation failed
@@ -574,11 +519,16 @@ public final class CRIUSupport {
 		/* Add env variables restore hook */
 		registerRestoreEnvVariables();
 
-		if (isCRIUSupportEnabled()) {
+		if (InternalCRIUSupport.isCheckpointAllowed()) {
 			checkpointJVMImpl(imageDir, leaveRunning, shellJob, extUnixSupport, logLevel, logFile, fileLocks, workDir,
 					tcpEstablished, autoDedup, trackMemory);
 		} else {
-			throw new UnsupportedOperationException("CRIU support is not enabled");
+			if (InternalCRIUSupport.isCRIUSupportEnabled()) {
+				throw new UnsupportedOperationException(
+					"Running in non-portable mode (only one checkpoint is allowed), and we have already checkpointed once"); //$NON-NLS-1$
+			} else {
+				throw new UnsupportedOperationException("CRIU support is not enabled"); //$NON-NLS-1$
+			}
 		}
 	}
 }
