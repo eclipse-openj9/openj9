25ce835b36b94a4e29ce1fbb68820f891dea1058
Peter Shipton <Peter_Shipton@ca.ibm.com>
2022-03-29 08:48:02 -0400

    Fix zlib compile issues
    Signed-off-by: Peter Shipton <Peter_Shipton@ca.ibm.com>

--- runtime/zlib/deflate.h
+++ runtime/zlib/deflate.h
@@ -329,11 +329,12 @@ void ZLIB_INTERNAL _tr_stored_block OF((deflate_state *s, charf *buf,
     s->dyn_ltree[cc].Freq++; \
     flush = (s->sym_next == s->sym_end); \
    }
+/* Source is modified from the original zlib version so it will compile on Windows. */
 # define _tr_tally_dist(s, distance, length, flush) \
   { uch len = (uch)(length); \
     ush dist = (ush)(distance); \
-    s->sym_buf[s->sym_next++] = dist; \
-    s->sym_buf[s->sym_next++] = dist >> 8; \
+    s->sym_buf[s->sym_next++] = (uch)dist; \
+    s->sym_buf[s->sym_next++] = (uch)(dist >> 8); \
     s->sym_buf[s->sym_next++] = len; \
     dist--; \
     s->dyn_ltree[_length_code[len]+LITERALS+1].Freq++; \
