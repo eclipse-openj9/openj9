d076c5fdfb6164a2eff60c52e9f500d4f407c2e3
midronij <jackie.midroni@ibm.com>
2022-04-11 11:22:17 -0400

    Handle vsqrt IL Opcode (added in omr)
    Signed-off-by: midronij <jackie.midroni@ibm.com>

--- runtime/compiler/optimizer/VectorAPIExpansion.cpp
+++ runtime/compiler/optimizer/VectorAPIExpansion.cpp
@@ -1577,7 +1577,7 @@ TR::ILOpCodes TR_VectorAPIExpansion::ILOpcodeFromVectorAPIOpcode(int32_t vectorA
       {
       case VECTOR_OP_ABS: return scalar ? TR::ILOpCode::absOpCode(elementType) : TR::ILOpCode::createVectorOpCode(OMR::vabs, vectorType);
       case VECTOR_OP_NEG: return scalar ? TR::ILOpCode::negateOpCode(elementType) : TR::ILOpCode::createVectorOpCode(OMR::vneg, vectorType);
-      case VECTOR_OP_SQRT:return TR::BadILOp;
+      case VECTOR_OP_SQRT:return scalar ? TR::BadILOp : TR::ILOpCode::createVectorOpCode(OMR::vsqrt, vectorType);
       case VECTOR_OP_ADD: return scalar ? TR::ILOpCode::addOpCode(elementType, true) : TR::ILOpCode::createVectorOpCode(OMR::vadd, vectorType);
       case VECTOR_OP_SUB: return scalar ? TR::ILOpCode::subtractOpCode(elementType) : TR::ILOpCode::createVectorOpCode(OMR::vsub, vectorType);
       case VECTOR_OP_MUL: return scalar ? TR::ILOpCode::multiplyOpCode(elementType) : TR::ILOpCode::createVectorOpCode(OMR::vmul, vectorType);
