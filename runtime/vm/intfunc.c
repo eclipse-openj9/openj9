2aa69795775a40404c04cc8e462353f03f385d41
Gengchen Tuo <gengchen.tuo@ibm.com>
2022-08-19 10:23:26 -0400

    Update jvmtiGetFrameCount to support virtual threads
    Adds `Assert_JVMTI_unreachable` to mark places as unreachable
    Adds `walkContinuationStackFrames` to internal functions so jvmti code can use it.
    Adds a check for whether it's a virtual thread and not yielded or terminated.
    If it is, use `walkContinuationStackFrames` to walk the continuation object.
    Issue: https://github.com/eclipse-openj9/openj9/issues/15183 https://github.com/eclipse-openj9/openj9/issues/15759
    
    Signed-off-by: Gengchen Tuo <gengchen.tuo@ibm.com>

--- runtime/vm/intfunc.c
+++ runtime/vm/intfunc.c
@@ -432,4 +432,7 @@ J9InternalVMFunctions J9InternalFunctions = {
 	native2InterpJavaUpcallStruct,
 */
 #endif /* JAVA_SPEC_VERSION >= 16 */
+#if JAVA_SPEC_VERSION >= 19
+	walkContinuationStackFrames,
+#endif /* JAVA_SPEC_VERSION >= 19 */
 };
