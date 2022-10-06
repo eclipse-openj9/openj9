20e1bc57fc4f7d542689a36256eef102f5f41f1b
Rahil Shah <rahil@ca.ibm.com>
2022-10-06 14:48:23 -0400

    Properly close the #IF/#ELSE/#ELSEIF macros in comment
    Signed-off-by: Rahil Shah <rahil@ca.ibm.com>

--- runtime/compiler/z/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/z/codegen/J9TreeEvaluator.cpp
@@ -8123,9 +8123,9 @@ reservationLockEnter(TR::Node *node, int32_t lwOffset, TR::Register *objectClass
       // BRC   MASK6, callHelper
       // #IF 64-Bit and JAVA_VERSION >= 19
       //    BRC   incrementOwnedMonitorCountLabel
-      // #ELSEIF
+      // #ELSE
       //    BRC   returnLabel
-      // BRC   returnLabel
+      // #ENDIF
       // checkLabel:
       // LGFI  tempReg, LOCK_RES_NON_PRIMITIVE_ENTER_MASK
       // NR    tempReg, monitorReg
@@ -8135,6 +8135,7 @@ reservationLockEnter(TR::Node *node, int32_t lwOffset, TR::Register *objectClass
       // #IF 64-Bit && JAVA_VERSION >=19
       //    incrementOwnedMonitorCountLabel:
       //    AGSI #ownedMonitorCountOffset(J9VMThread), 1
+      // #ENDIF
       // BRC   returnLabel
       // callHelper:
       // BRASL R14, jitMonitorEntry
@@ -8316,6 +8317,7 @@ reservationLockExit(TR::Node *node, int32_t lwOffset, TR::Register *objectClassR
    //   ST    valReg, #lwOffset(objectReg)
    // #IF 64-Bit && JAVA_VERSION >=19
    //    AGSI #ownedMonitorCountOffset(J9VMThread), -1
+   // #ENDIF
 
    generateRXInstruction(cg, loadOp, node, monitorReg, generateS390MemoryReference(objReg, lwOffset, cg));
    if (!isPrimitive)
@@ -8384,6 +8386,7 @@ reservationLockExit(TR::Node *node, int32_t lwOffset, TR::Register *objectClassR
       // ST    monitorReg, #lwOffset(objectReg)
       // #IF 64-Bit && JAVA_VERSION >=19
       //    AGSI #ownedMonitorCountOffset(J9VMThread), -1
+      // #ENDIF
       // BRC   returnLabel
       // callHelper:
       // BRASL R14, jitMonitorExit
