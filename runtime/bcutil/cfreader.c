7037a517283345c13cf7d0bfe01ff4353e8cce8f
Hang Shao <hangshao@ca.ibm.com>
2022-10-12 16:11:44 -0400

    Update JDK version for Valhalla
    1. Valhalla is built on JDKNext which is Java 20 now. Update the code to
    check against Java 20.
    2. Update the test code to generate Java 20 classes.
    
    Signed-off-by: Hang Shao <hangshao@ca.ibm.com>

--- runtime/bcutil/cfreader.c
+++ runtime/bcutil/cfreader.c
@@ -3011,8 +3011,8 @@ j9bcutil_readClassFileBytes(J9PortLibrary *portLib,
 	}
 
 #if defined(J9VM_OPT_VALHALLA_VALUE_TYPES)
-	/* Currently value type is built on JDK19, so compare with JDK19 for now. Eventually it may need to compare to JDK20. */
-	if ((flags & BCT_MajorClassFileVersionMask) < BCT_JavaMajorVersionShifted(19)) {
+	/* Currently value type is built on JDK20, so compare with JDK20 for now. Eventually it may need to compare to JDK21. */
+	if ((flags & BCT_MajorClassFileVersionMask) < BCT_JavaMajorVersionShifted(20)) {
 		classfile->accessFlags &= ~(CFR_ACC_VALUE_TYPE | CFR_ACC_PRIMITIVE_VALUE_TYPE | CFR_ACC_IDENTITY);
 	}
 	if (J9_ARE_ALL_BITS_SET(classfile->accessFlags, CFR_ACC_PRIMITIVE_VALUE_TYPE)) {
