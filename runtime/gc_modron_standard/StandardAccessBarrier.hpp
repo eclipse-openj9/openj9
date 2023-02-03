a4e7f275ecb630ead9c5ecccfc8e1dfa9dfae0a5
Dmitri Pivkine <Dmitri_Pivkine@ca.ibm.com>
2023-02-03 13:51:22 -0500

    Prepare to move Scavenger creation to createGlobalCollector()
    Current place of MM_Scavenger instantiation is
    MM_ConfigurationGenerational::createDefaultMemorySpace(). It is
    relatively late in initialization path without any reason. Late creation
    required to have "Scavenger registration" in the classes created
    earlier. Moving Scavenger instantiation to
    MM_ConfigurationGenerational::createGlobalCollector() makes more sense
    and resolves registration problem naturally. Method
    createGlobalCollector() is going to be renamed to createCollectors() to
    reflect the fact it might create not only Global, but Local collector
    too if necessary.
    
    There is first step of refactoring:
    
    - add _scavenger field initialization to Constructor of
    MM_StandardAccessBarrier
    - move _extensions and _heap fields initialization to Constructor of
    MM_ObjectaccessBarrier
    - duplicate createGlobalCollector() to creteCollectors() for Balanced
    and RealTime Configurations as a preparation to rename the method
    
    Signed-off-by: Dmitri Pivkine <Dmitri_Pivkine@ca.ibm.com>

--- runtime/gc_modron_standard/StandardAccessBarrier.hpp
+++ runtime/gc_modron_standard/StandardAccessBarrier.hpp
@@ -1,6 +1,6 @@
 
 /*******************************************************************************
- * Copyright (c) 1991, 2022 IBM Corp. and others
+ * Copyright (c) 1991, 2023 IBM Corp. and others
  *
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
@@ -72,7 +72,7 @@ public:
 	MM_StandardAccessBarrier(MM_EnvironmentBase *env, MM_MarkingScheme *markingScheme) :
 		MM_ObjectAccessBarrier(env)
 #if defined(J9VM_GC_GENERATIONAL)
-		, _scavenger(NULL)
+		, _scavenger(_extensions->scavenger)
 #endif /* J9VM_GC_GENERATIONAL */
 		, _markingScheme(markingScheme)
 	{
