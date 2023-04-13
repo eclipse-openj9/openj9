8d26b263cb180d55f5eb254fa213946ff526f83a
LongyuZhang <longyu.zhang@ibm.com>
2023-04-13 13:24:36 -0400

    Fix cmdLineTests for JVM option setting - Fix cmdLineTests for JVM option setting
    Signed-off-by: LongyuZhang <longyu.zhang@ibm.com>

--- test/functional/cmdLineTests/variables.mk
+++ test/functional/cmdLineTests/variables.mk
@@ -20,4 +20,7 @@
 # SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
 ##############################################################################
 
-CMDLINETESTER_JVM_OPTIONS=$(Q)-Xshareclasses:none$(Q)
+# if JDK_IMPL is openj9 or ibm
+ifneq ($(filter openj9 ibm, $(JDK_IMPL)),)
+    CMDLINETESTER_JVM_OPTIONS=$(Q)-Xshareclasses:none$(Q)
+endif
