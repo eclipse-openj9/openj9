833e1b6bacaa63844971a1c999661560996522b8
Jack Lu <Jack.S.Lu@ibm.com>
2023-05-02 14:46:58 -0400

    Add FastJNI impl for VirtualThread.notifyJvmti* calls
    - Move Critical region control to ContinuationHelpers
    
    Signed-off-by: Jack Lu <Jack.S.Lu@ibm.com>

--- runtime/vm/FastJNI.cpp
+++ runtime/vm/FastJNI.cpp
@@ -49,6 +49,9 @@ J9_FAST_JNI_METHOD_TABLE_EXTERN(java_lang_String);
 J9_FAST_JNI_METHOD_TABLE_EXTERN(java_lang_reflect_Array);
 J9_FAST_JNI_METHOD_TABLE_EXTERN(java_lang_Throwable);
 J9_FAST_JNI_METHOD_TABLE_EXTERN(java_lang_invoke_MethodHandle);
+#if JAVA_SPEC_VERSION >= 20
+J9_FAST_JNI_METHOD_TABLE_EXTERN(java_lang_VirtualThread);
+#endif /* JAVA_SPEC_VERSION >= 20 */
 
 J9_FAST_JNI_CLASS_TABLE(fastJNINatives)
 	J9_FAST_JNI_CLASS("java/lang/J9VMInternals", java_lang_J9VMInternals)
@@ -65,6 +68,9 @@ J9_FAST_JNI_CLASS_TABLE(fastJNINatives)
 	J9_FAST_JNI_CLASS("java/lang/reflect/Array", java_lang_reflect_Array)
 	J9_FAST_JNI_CLASS("java/lang/Throwable", java_lang_Throwable)
 	J9_FAST_JNI_CLASS("java/lang/invoke/MethodHandle", java_lang_invoke_MethodHandle)
+#if JAVA_SPEC_VERSION >= 20
+	J9_FAST_JNI_CLASS("java/lang/VirtualThread", java_lang_VirtualThread)
+#endif /* JAVA_SPEC_VERSION >= 20 */
 J9_FAST_JNI_CLASS_TABLE_END
 
 static bool
