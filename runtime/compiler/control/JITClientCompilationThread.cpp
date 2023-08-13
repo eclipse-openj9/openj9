75afd20e48b1dcb5c7310ef9df421967040deefc
Marius Pirvu <mpirvu@ca.ibm.com>
2023-08-12 20:13:06 -0400

    Add missing break statement for switch case
    Signed-off-by: Marius Pirvu <mpirvu@ca.ibm.com>

--- runtime/compiler/control/JITClientCompilationThread.cpp
+++ runtime/compiler/control/JITClientCompilationThread.cpp
@@ -1104,6 +1104,7 @@ handleServerMessage(JITServer::ClientStream *client, TR_J9VM *fe, JITServer::Mes
          auto recv = client->getRecvData<uintptr_t *>();
          client->write(response, fe->isInvokeCacheEntryAnArray(std::get<0>(recv)));
          }
+         break;
       case MessageType::VM_getKnotIndexOfInvokeCacheArrayAppendixElement:
          {
          auto recv = client->getRecvData<uintptr_t *>();
