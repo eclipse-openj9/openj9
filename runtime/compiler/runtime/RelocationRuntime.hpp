db88dc1e5b36a3cfd0c798b0011f0c608fdce649
Christian Despres <despresc@ibm.com>
2023-09-28 15:42:30 +0000

    Move AOT header filling to base relo runtime
    The new TR_RelocationRuntime::fillAOTHeader is responsible for
    filling in a new TR_AOTHeader. Actual allocation of this header is
    still handled by TR_SharedCacheRelocationRuntime::createAOTHeader.
    
    Signed-off-by: Christian Despres <despresc@ibm.com>

--- runtime/compiler/runtime/RelocationRuntime.hpp
+++ runtime/compiler/runtime/RelocationRuntime.hpp
@@ -301,6 +301,8 @@ class TR_RelocationRuntime {
       virtual bool validateAOTHeader(TR_FrontEnd *fe, J9VMThread *curThread);
       virtual OMRProcessorDesc getProcessorDescriptionFromSCC(J9VMThread *curThread);
 
+      static void fillAOTHeader(J9JavaVM *vm, TR_FrontEnd *fe, TR_AOTHeader *header);
+
       static uintptr_t   getGlobalValue(uint32_t g)
          {
          TR_ASSERT(g >= 0 && g < TR_NumGlobalValueItems, "invalid index for global item");
@@ -394,6 +396,8 @@ class TR_RelocationRuntime {
       static const char *_reloErrorCodeNames[];
 
    protected:
+      static uintptr_t generateFeatureFlags(TR_FrontEnd *fe);
+      static uint32_t getCurrentLockwordOptionHashValue(J9JavaVM *vm);
 
       J9JITConfig *_jitConfig;
       J9JavaVM *_javaVM;
@@ -470,7 +474,6 @@ public:
       virtual OMRProcessorDesc getProcessorDescriptionFromSCC(J9VMThread *curThread);
 
 private:
-      uint32_t getCurrentLockwordOptionHashValue(J9JavaVM *vm) const;
       virtual uint8_t * allocateSpaceInCodeCache(UDATA codeSize);
       virtual uint8_t * allocateSpaceInDataCache(UDATA metaDataSize, UDATA type);
       virtual void initializeAotRuntimeInfo();
@@ -482,8 +485,6 @@ private:
       bool generateError(U_32 module_name, U_32 reason, const char *assumeMessage);
 
       bool _sharedCacheIsFull;
-
-      static uintptr_t generateFeatureFlags(TR_FrontEnd *fe);
 
       static const char aotHeaderKey[];
       static const UDATA aotHeaderKeyLength;
