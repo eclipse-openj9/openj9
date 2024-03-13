3d11527e5573edb9715299a7308205824c890a77
Christian Despres <despresc@ibm.com>
2024-03-13 16:41:30 +0000

    Add initial server connection flag
    The new _hasConnectedToServer flag in the persistent info is true at a
    JITServer client when it has previously connected to a server,
    regardless of the current status of any server connection. Tracking
    this property is necessary to ensure that the deserializer is reset
    on new connections, as the previous method (checking if the serverUID is
    0) is not reliable for that purpose.
    
    This fixes a bug that prevented the deserializer from being reset when
    a client connected to a new server.
    
    Signed-off-by: Christian Despres <despresc@ibm.com>

--- runtime/compiler/env/J9PersistentInfo.hpp
+++ runtime/compiler/env/J9PersistentInfo.hpp
@@ -165,6 +165,7 @@ class PersistentInfo : public OMR::PersistentInfoConnector
          _socketTimeoutMs(0),
          _clientUID(0),
          _serverUID(0),
+         _hasEverConnectedToServer(false),
          _JITServerMetricsPort(38500),
          _JITServerHealthPort(38600),
          _requireJITServer(false),
@@ -355,6 +356,8 @@ class PersistentInfo : public OMR::PersistentInfoConnector
    void setClientUID(uint64_t val) { _clientUID = val; }
    uint64_t getServerUID() const { return _serverUID; }
    void setServerUID(uint64_t val) { _serverUID = val; }
+   bool hasEverConnectedToServer() const { return _hasEverConnectedToServer; }
+   void setHasEverConnectedToServer() { _hasEverConnectedToServer = true; }
    uint32_t getJITServerMetricsPort() const { return _JITServerMetricsPort; }
    void setJITServerMetricsPort(uint32_t port) { _JITServerMetricsPort = port; }
    uint32_t getJITServerHealthPort() const { return _JITServerHealthPort; }
@@ -469,6 +472,7 @@ class PersistentInfo : public OMR::PersistentInfoConnector
    uint32_t    _socketTimeoutMs; // timeout for communication sockets used in out-of-process JIT compilation
    uint64_t    _clientUID;
    uint64_t    _serverUID; // At the client, this represents the UID of the server the client is connected to
+   bool        _hasEverConnectedToServer; // At the client, true if the client has connected to a server at some point in the past
    uint32_t    _JITServerMetricsPort; // Port for receiving http metrics requests from Prometheus; only used at server
    uint32_t    _JITServerHealthPort; // Port for receiving readiness/liveness probes from Kubernetes; only used at server
    bool        _requireJITServer;
