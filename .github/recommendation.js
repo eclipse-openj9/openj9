d38e81765a2b789844912652ea7b56fd46b0669e
Diyorbek Ibragimov <59175919+diyorbekibragimov@users.noreply.github.com>
2024-07-07 18:42:58 +0500

    Removed console.logs

--- .github/recommendation.js
+++ .github/recommendation.js
@@ -27,13 +27,6 @@ async function run() {
         predictedAssignees = issueComment.recommended_developers;
         predictedLabels = issueComment.recommended_components;
 
-        console.log(process.env.ISSUE_NUMBER);
-        console.log(typeof process.env.ISSUE_NUMBER);
-        console.log(`Issue number: ${process.env.ISSUE_NUMBER}`);
-
-        console.log(input);
-
-        resultString = `Issue Number: ${process.env.ISSUE_NUMBER}\n`;
         resultString += 'Status: Open\n';
         resultString += `Recommended Components: ${predictedLabels.join(', ')}\n`;
         resultString += `Recommended Assignees: ${predictedAssignees.join(', ')}\n`;
