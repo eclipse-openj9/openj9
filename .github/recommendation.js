84782544c7b41b804342e3e9448fabb5964d6ae8
Bel Ami Gisage Warakoze <125438939+Belami02@users.noreply.github.com>
2024-07-07 14:39:41 +0300

    Updated the file with async function

--- .github/recommendation.js
+++ .github/recommendation.js
@@ -5,34 +5,36 @@ const apiUrl = "http://140.211.168.122/recommendation";
 const sandboxIssueNumber = 1;
 const sandboxOwner = context.repo.owner;
 const sandboxRepo = context.repo.repo;
+async function run() {
+  try {
+    const response = await axios.post(apiUrl, JSON.stringify(input), {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }
+    });
 
-try {
-  const response = await axios.post(apiUrl, JSON.stringify(input), {
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
+    const issueComment = response.data;
+    predictedAssignees = issueComment.recommended_developers;
+    predictedLabels = issueComment.recommended_components;
+
+    resultString = `Issue Number: ${issueNumber}\n`;
+    resultString += 'Status: Open\n';
+    resultString += `Recommended Components: ${predictedLabels.join(', ')}\n`;
+    resultString += `Recommended Assignees: ${predictedAssignees.join(', ')}\n`;
+    await github.issues.createComment({
+      issue_number: sandboxIssueNumber,
+      owner: sandboxOwner,
+      repo: sandboxRepo,
+      body: resultString
+    });
+  } catch (error) {
+    await github.issues.createComment({
+      issue_number: sandboxIssueNumber,
+      owner: sandboxOwner,
+      repo: sandboxRepo,
+      body: `The TriagerX model is currently not responding to the issue ${issueNumber}. Please try again later.`
+    });
   }
-});
-
-const issueComment = response.data;
-predictedAssignees = issueComment.recommended_developers;
-predictedLabels = issueComment.recommended_components;
-
-resultString = `Issue Number: ${issueNumber}\n`;
-resultString += 'Status: Open\n';
-resultString += `Recommended Components: ${predictedLabels.join(', ')}\n`;
-resultString += `Recommended Assignees: ${predictedAssignees.join(', ')}\n`;
-await github.issues.createComment({
-  issue_number: sandboxIssueNumber,
-  owner: sandboxOwner,
-  repo: sandboxRepo,
-  body: resultString
-  });
-} catch (error) {
-  await github.issues.createComment({
-    issue_number: sandboxIssueNumber,
-    owner: sandboxOwner,
-    repo: sandboxRepo,
-    body: `The TriagerX model is currently not responding to the issue ${issueNumber}. Please try again later.`
-  });
 }
+run();
