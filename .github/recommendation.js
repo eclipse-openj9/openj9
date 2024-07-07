7cdd4ef778b5a9e51522c08fdf6f3bb05f84d356
Diyorbek Ibragimov <59175919+diyorbekibragimov@users.noreply.github.com>
2024-07-07 17:51:11 +0500

    Added the issue number

--- .github/recommendation.js
+++ .github/recommendation.js
@@ -26,10 +26,13 @@ async function run() {
     predictedAssignees = issueComment.recommended_developers;
     predictedLabels = issueComment.recommended_components;
 
+    const issueNumber = process.env.ISSUE_NUMBER;
+
     resultString = `Issue Number: ${issueNumber}\n`;
     resultString += 'Status: Open\n';
     resultString += `Recommended Components: ${predictedLabels.join(', ')}\n`;
     resultString += `Recommended Assignees: ${predictedAssignees.join(', ')}\n`;
+    
     await github.issues.createComment({
       issue_number: sandboxIssueNumber,
       owner: sandboxOwner,
