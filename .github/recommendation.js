463668064b7838b8821a09b3192efa8e3ca5124d
George Chkhaidze <giochkhaidze10@gmail.com>
2024-07-22 06:26:20 +0400

    Fixing bugs in recommendation.js

--- .github/recommendation.js
+++ .github/recommendation.js
@@ -24,9 +24,11 @@ async function run() {
         });
 
         const issueComment = response.data;
+        const issueNumber = process.env.ISSUE_NUMBER;
         predictedAssignees = issueComment.recommended_developers;
         predictedLabels = issueComment.recommended_components;
 
+        let resultString = `Issue Number: ${issueNumber}\n`;
         resultString += 'Status: Open\n';
         resultString += `Recommended Components: ${predictedLabels.join(', ')}\n`;
         resultString += `Recommended Assignees: ${predictedAssignees.join(', ')}\n`;
