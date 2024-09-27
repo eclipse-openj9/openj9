30a47ed7b76c2a984a4c768334e0cdf5e0ceefb4
Abdulrahman Alattas <rmnattas@gmail.com>
2024-09-27 15:15:44 -0400

    Use baseObj for Unsafe.CAS CardMarking for OffHeap on P
    When evaluating Unsafe.CAS while running with Balanced GC and OffHeap
    enabled, if the object child is the dataAddrPointer load, pass the
    baseObj to VMCardCheckEvaluator for correct card marking.
    
    The dstReg and temp2Reg in VMCardCheckEvaluator can share a reg.
    
    Signed-off-by: Abdulrahman Alattas <rmnattas@gmail.com>

--- runtime/compiler/p/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/p/codegen/J9TreeEvaluator.cpp
@@ -8217,6 +8217,12 @@ static TR::Register *VMinlineCompareAndSwapObject(TR::Node *node, TR::CodeGenera
    thirdChild = node->getChild(2);
    fourthChild = node->getChild(3);
    fifthChild = node->getChild(4);
+#if defined(J9VM_GC_ENABLE_SPARSE_HEAP_ALLOCATION)
+   bool isObjOffHeapDataAddr = (gcMode == gc_modron_wrtbar_cardmark_incremental && TR::Compiler->om.isOffHeapAllocationEnabled() && secondChild->isDataAddrPointer());
+   TR::Register *baseObjReg = NULL;
+   if (isObjOffHeapDataAddr)
+      TR::TreeEvaluator::stopUsingCopyReg(secondChild->getFirstChild(), baseObjReg, cg);
+#endif /* defined(J9VM_GC_ENABLE_SPARSE_HEAP_ALLOCATION) */
    objReg = cg->evaluate(secondChild);
 
    // VM helper chops off the value in 32bit, and we don't want the whole long value either
@@ -8473,7 +8479,13 @@ static TR::Register *VMinlineCompareAndSwapObject(TR::Node *node, TR::CodeGenera
       }
    else if (!doWrtBar && doCrdMrk)
       {
-      TR::Register *temp1Reg = cg->allocateRegister(), *temp2Reg = cg->allocateRegister(), *temp3Reg;
+      TR::Register *temp1Reg = cg->allocateRegister(), *temp2Reg, *temp3Reg;
+#if defined(J9VM_GC_ENABLE_SPARSE_HEAP_ALLOCATION)
+      if (isObjOffHeapDataAddr)
+         temp2Reg = baseObjReg; // Use baseObjReg as temp2Reg
+      else
+#endif /* defined(J9VM_GC_ENABLE_SPARSE_HEAP_ALLOCATION) */
+         temp2Reg = cg->allocateRegister();
       TR::addDependency(conditions, objReg, TR::RealRegister::NoReg, TR_GPR, cg);
       conditions->getPostConditions()->getRegisterDependency(0)->setExcludeGPR0();
       TR::addDependency(conditions, temp1Reg, TR::RealRegister::NoReg, TR_GPR, cg);
@@ -8494,7 +8506,13 @@ static TR::Register *VMinlineCompareAndSwapObject(TR::Node *node, TR::CodeGenera
          TR::addDependency(conditions, temp3Reg, TR::RealRegister::NoReg, TR_GPR, cg);
          }
 
-      VMCardCheckEvaluator(node, objReg, cndReg, temp1Reg, temp2Reg, temp3Reg, conditions, cg);
+#if defined(J9VM_GC_ENABLE_SPARSE_HEAP_ALLOCATION)
+      if (isObjOffHeapDataAddr)
+         // Given that baseObjReg is trash-able it can be used as the objReg and temp2Reg
+         VMCardCheckEvaluator(node, baseObjReg, cndReg, temp1Reg, temp2Reg, temp3Reg, conditions, cg);
+      else
+#endif /* defined(J9VM_GC_ENABLE_SPARSE_HEAP_ALLOCATION) */
+         VMCardCheckEvaluator(node, objReg, cndReg, temp1Reg, temp2Reg, temp3Reg, conditions, cg);
 
       cg->stopUsingRegister(temp1Reg);
       cg->stopUsingRegister(temp2Reg);
