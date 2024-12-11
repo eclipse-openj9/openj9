032eabb8cde4c62636497ab1178a0d076937f269
lhu <linhu@ca.ibm.com>
2024-12-11 00:11:39 -0500

    Enable off-heap as default on Balanced GC
    Enable off-heap as default only Balanced GC
     1, global flag isVirtualLargeObjectHeapEnabled = true on Balanced GC by
    default.
     2, if user specify -XXgc:disableVirtualLargeObjectHeap on Balanced GC
     off heap can be disabled.
     3, isVirtualLargeObjectHeapEnabled = false on the rest of
     gcpolicies.
     4, -XXgc:disableVirtualLargeObjectHeap/enableVirtualLargeObjectHeap
     would be ignored on  the rest of gcpolicies.
     5, if user specify -XXgc:enableVirtualLargeObjectHeap on
     gcpolicy:metronome, jvm would output warmomg message
    
    Signed-off-by: lhu <linhu@ca.ibm.com>

--- runtime/gc_vlhgc/ConfigurationIncrementalGenerational.cpp
+++ runtime/gc_vlhgc/ConfigurationIncrementalGenerational.cpp
@@ -98,7 +98,15 @@ MM_ConfigurationIncrementalGenerational::createHeapWithManager(MM_EnvironmentBas
 	if (NULL == heap) {
 		return NULL;
 	}
-
+#if defined(J9VM_GC_SPARSE_HEAP_ALLOCATION)
+	/* set off-heap enabled as default for balanced GC */
+#if !defined(J9ZTPF)
+	extensions->isVirtualLargeObjectHeapEnabled = true;
+#endif /* !defined(J9ZTPF) */
+	if (extensions->virtualLargeObjectHeap._wasSpecified) {
+		extensions->isVirtualLargeObjectHeapEnabled = extensions->virtualLargeObjectHeap._valueSpecified;
+	}
+#endif /* defined(J9VM_GC_SPARSE_HEAP_ALLOCATION) */
 #if defined(J9VM_GC_ENABLE_DOUBLE_MAP)
 	/* Enable double mapping if glibc version 2.27 or newer is found. For double map to
 	 * work we need a file descriptor, to get one we use shm_open(3)  or memfd_create(2);
@@ -113,7 +121,7 @@ MM_ConfigurationIncrementalGenerational::createHeapWithManager(MM_EnvironmentBas
 	 * also need to check if region size is a bigger or equal to multiple of page size.
 	 *
 	 */
-	if (!extensions->isVirtualLargeObjectHeapRequested && extensions->isArrayletDoubleMapRequested && extensions->isArrayletDoubleMapAvailable) {
+	if (!extensions->isVirtualLargeObjectHeapEnabled && extensions->isArrayletDoubleMapRequested && extensions->isArrayletDoubleMapAvailable) {
 		uintptr_t pagesize = heap->getPageSize();
 		if (!extensions->memoryManager->isLargePage(env, pagesize) || (pagesize <= extensions->getOmrVM()->_arrayletLeafSize)) {
 			extensions->indexableObjectModel.setEnableDoubleMapping(true);
@@ -164,13 +172,12 @@ MM_ConfigurationIncrementalGenerational::createHeapWithManager(MM_EnvironmentBas
 	 */
 	vm->indexableObjectLayout = J9IndexableObjectLayout_DataAddr_Arraylet;
 #if defined(J9VM_GC_SPARSE_HEAP_ALLOCATION)
-	if (extensions->isVirtualLargeObjectHeapRequested) {
+	if (extensions->isVirtualLargeObjectHeapEnabled) {
 		/* Create off-heap */
 		MM_SparseVirtualMemory *largeObjectVirtualMemory = MM_SparseVirtualMemory::newInstance(env, OMRMEM_CATEGORY_MM_RUNTIME_HEAP, heap);
 		if (NULL != largeObjectVirtualMemory) {
 			extensions->largeObjectVirtualMemory = largeObjectVirtualMemory;
 			extensions->indexableObjectModel.setEnableVirtualLargeObjectHeap(true);
-			extensions->isVirtualLargeObjectHeapEnabled = true;
 			/* Overriding the original assumption that Balanced has arraylets. */
 			vm->indexableObjectLayout = J9IndexableObjectLayout_DataAddr_NoArraylet;
 			/* reset vm->unsafeIndexableHeaderSize for off-heap case */
