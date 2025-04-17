5eadcf9c81d46ac8194687ec3ef5b35b9a93d7aa
Tobi Ajila <atobia@ca.ibm.com>
2025-04-17 11:32:22 -0400

    Add support for JNI sync method
    Currently, JNI monenter path assumes its returning from a monitor enter
    which is incorrect. Instead it must restore the JNI frame and restart
    the native invocation.
    
    https://github.com/eclipse-openj9/openj9/issues/21424 no longer crashes.
    
    Signed-off-by: Tobi Ajila <atobia@ca.ibm.com>

--- runtime/vm/BytecodeInterpreter.hpp
+++ runtime/vm/BytecodeInterpreter.hpp
@@ -2439,7 +2439,7 @@ done:
 #endif /* defined(J9VM_OPT_CRIU_SUPPORT) */
 #if JAVA_SPEC_VERSION >= 24
 				case J9_OBJECT_MONITOR_YIELD_VIRTUAL: {
-					rc = yieldPinnedContinuation(REGISTER_ARGS, JAVA_LANG_VIRTUALTHREAD_BLOCKING, J9VM_CONTINUATION_RETURN_FROM_MONITOR_ENTER);
+					rc = yieldPinnedContinuation(REGISTER_ARGS, JAVA_LANG_VIRTUALTHREAD_BLOCKING, J9VM_CONTINUATION_RETURN_FROM_SYNC_METHOD_JNI);
 					break;
 				}
 #endif /* JAVA_SPEC_VERSION >= 24 */
@@ -5843,6 +5843,13 @@ ffi_OOM:
 					returnVoidFromINL(REGISTER_ARGS, 4);
 				}
 			}
+			break;
+		}
+		case J9VM_CONTINUATION_RETURN_FROM_SYNC_METHOD_JNI: {
+			UDATA *bp = (UDATA *)_sp + 4;
+			_sendMethod = *(J9Method **)_sp;
+			restoreSpecialStackFrameLeavingArgs(REGISTER_ARGS, bp);
+			rc = RUN_JNI_NATIVE;
 			break;
 		}
 		case J9VM_CONTINUATION_RETURN_FROM_SYNC_METHOD: {
