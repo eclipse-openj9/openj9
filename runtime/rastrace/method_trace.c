9c6d3b2d998359d2125ad2c1f66497028101220a
Nick Kamal <nick.kamal@ibm.com>
2025-06-04 20:05:23 -0400

    Print string contents for "this" references
    The changes reflect the feature request #20878.
    
    Print string contents for this references.
    Also, print address for string arguments and returns.
    
    Closes: #20878
    Signed-off-by: Nick Kamal <nick.kamal@ibm.com>

--- runtime/rastrace/method_trace.c
+++ runtime/rastrace/method_trace.c
@@ -173,7 +173,7 @@ traceMethodEnter(J9VMThread *thr, J9Method *method, void *receiverAddress, UDATA
 	J9UTF8* methodSignature = J9ROMMETHOD_SIGNATURE(J9_ROM_METHOD_FROM_RAM_METHOD(method));
 	UDATA modifiers = J9_ROM_METHOD_FROM_RAM_METHOD(method)->modifiers;
 
-	if (isCompiled) {
+	if (0 != isCompiled) {
 		if (modifiers & J9AccStatic) {
 			Trc_MethodEntryCS(thr, J9UTF8_LENGTH(className), J9UTF8_DATA(className), J9UTF8_LENGTH(methodName), J9UTF8_DATA(methodName), J9UTF8_LENGTH(methodSignature), J9UTF8_DATA(methodSignature));
 		} else {
@@ -189,7 +189,7 @@ traceMethodEnter(J9VMThread *thr, J9Method *method, void *receiverAddress, UDATA
 			} else {
 				Trc_MethodEntryS(thr, J9UTF8_LENGTH(className), J9UTF8_DATA(className), J9UTF8_LENGTH(methodName), J9UTF8_DATA(methodName), J9UTF8_LENGTH(methodSignature), J9UTF8_DATA(methodSignature));
 			}
-			if (doParameters) {
+			if (0 != doParameters) {
 				Trc_MethodArgumentsS(thr, traceMethodArguments(thr, methodSignature, (UDATA*)receiverAddress, buf, buf + sizeof(buf)));
 			}
 		} else {
@@ -203,13 +203,51 @@ traceMethodEnter(J9VMThread *thr, J9Method *method, void *receiverAddress, UDATA
 				Trc_MethodEntry(thr, J9UTF8_LENGTH(className), J9UTF8_DATA(className), J9UTF8_LENGTH(methodName), J9UTF8_DATA(methodName), J9UTF8_LENGTH(methodSignature), J9UTF8_DATA(methodSignature), receiver);
 			}
 
-			if (doParameters) {
+			if (0 != doParameters) {
 				Trc_MethodArguments(
-						thr, 
+						thr,
 						(U_32)J9UTF8_LENGTH(receiverClassName),
 						J9UTF8_DATA(receiverClassName),
 						receiver,
 						traceMethodArguments(thr, methodSignature, (UDATA*)receiverAddress - 1, buf, buf + sizeof(buf)));
+			}
+		}
+	}
+
+	if (0 != doParameters) {
+		j9object_t receiver = J9_ARE_ANY_BITS_SET(modifiers, J9AccStatic) ? NULL : *(j9object_t *)receiverAddress;
+		J9Class *receiverClazz = NULL == receiver ? NULL : J9OBJECT_CLAZZ(thr, receiver);
+		J9JavaVM *vm = thr->javaVM;
+		const unsigned int maxStringLength = RAS_GLOBAL_FROM_JAVAVM(maxStringLength, vm);
+
+		if ((receiverClazz == J9VMJAVALANGSTRING_OR_NULL(vm))
+			&& (0 != maxStringLength)
+			&& (!J9UTF8_LITERAL_EQUALS(J9UTF8_DATA(methodName), J9UTF8_LENGTH(methodName), "<init>"))
+		) {
+			PORT_ACCESS_FROM_VMC(thr);
+			char utf8Buffer[RAS_MAX_STRING_LENGTH_LIMIT + 1];
+			char outputString[RAS_MAX_STRING_LENGTH_LIMIT + 1];
+			UDATA utf8Length = 0;
+
+			char *utf8String = vm->internalVMFunctions->copyStringToUTF8WithMemAlloc(
+					thr,
+					receiver,
+					0,
+					"",
+					0,
+					utf8Buffer,
+					sizeof(utf8Buffer),
+					&utf8Length);
+			if (NULL == utf8String) {
+				j9str_printf(outputString, RAS_MAX_STRING_LENGTH_LIMIT + 1, "<Memory allocation error>");
+			} else if (utf8Length > maxStringLength) {
+				j9str_printf(outputString, RAS_MAX_STRING_LENGTH_LIMIT + 1, "\"%.*s\"...", (U_32)maxStringLength, utf8String);
+			} else {
+				j9str_printf(outputString, RAS_MAX_STRING_LENGTH_LIMIT + 1, "\"%.*s\"", (U_32)utf8Length, utf8String);
+			}
+			Trc_MethodArgumentsStr(thr, receiver, J9UTF8_DATA(methodSignature), outputString);
+			if (utf8Buffer != utf8String) {
+				j9mem_free_memory(utf8String);
 			}
 		}
 	}
@@ -496,11 +534,11 @@ traceMethodArgObject(J9VMThread *thr, UDATA* arg0EA, char* cursor, UDATA length)
 					&utf8Length);
 
 			if (NULL == utf8String) {
-				j9str_printf(cursor, length, "(String)<Memory allocation error>");
+				j9str_printf(cursor, length, "(String)@%p - <Memory allocation error>", object);
 			} else if (utf8Length > maxStringLength) {
-				j9str_printf(cursor, length, "(String)\"%.*s\"...", (U_32)maxStringLength, utf8String);
+				j9str_printf(cursor, length, "(String)@%p - \"%.*s\"...", object, (U_32)maxStringLength, utf8String);
 			} else {
-				j9str_printf(cursor, length, "(String)\"%.*s\"", (U_32)utf8Length, utf8String);
+				j9str_printf(cursor, length, "(String)@%p - \"%.*s\"", object, (U_32)utf8Length, utf8String);
 			}
 
 			if (utf8Buffer != utf8String) {
