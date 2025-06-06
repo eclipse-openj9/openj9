6512e7628a8b62b6236276fc20c9c6cded0ac373
KONNO Kazuhiro <konno@jp.ibm.com>
2025-06-06 14:06:00 +0900

    Recognize StringUTF16.indexOfUnsafe([BI[BII)I
    This commit changes the JIT compiler to recognize the method
    StringUTF16.indexOfUnsafe([BI[BII)I in addition to
    StringUTF16.indexOf([BI[BII)I.
    
    It improves the performance of String.indexOf(String) on Z and X with
     Java 17+.
    
    Signed-off-by: KONNO Kazuhiro <konno@jp.ibm.com>

--- runtime/compiler/optimizer/InlinerTempForJ9.cpp
+++ runtime/compiler/optimizer/InlinerTempForJ9.cpp
@@ -5634,6 +5634,7 @@ TR_J9InlinerPolicy::suppressInliningRecognizedInitialCallee(TR_CallSite* callsit
          break;
       case TR::java_lang_StringLatin1_indexOf:
       case TR::java_lang_StringUTF16_indexOf:
+      case TR::java_lang_StringUTF16_indexOfUnsafe:
       case TR::com_ibm_jit_JITHelpers_intrinsicIndexOfStringLatin1:
       case TR::com_ibm_jit_JITHelpers_intrinsicIndexOfStringUTF16:
          if (cg->getSupportsInlineStringIndexOfString())
