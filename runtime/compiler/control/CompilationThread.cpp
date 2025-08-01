ff253103f28d54973773f26c6b574d7711841ef8
Devin Papineau <devin@ajdmp.ca>
2025-08-01 11:53:39 -0400

    Make client and server agree on exact permanent loaders
    ...within each compilation.
    
    There will be at least two data structures that are sensitive to the
    set of permanent loaders. These could potentially allow the precise set
    of permanent loaders to vary between the client and server, but the
    reasoning needed to determine whether certain kinds of variation are
    allowed gets hairy very quickly. It's much simpler if there is only one
    set of permanent loaders that will be used for all purposes on both the
    client and the server.
    
    This change also simplifies reasoning about which permanent loaders the
    server already has. The client no longer attempts to keep track of that
    at all. Instead, that information is purely in the purview of the
    server, where it's easy to maintain consistency.

--- runtime/compiler/control/CompilationThread.cpp
+++ runtime/compiler/control/CompilationThread.cpp
@@ -8375,6 +8375,9 @@ TR::CompilationInfoPerThreadBase::compile(J9VMThread * vmThread,
          trMemory,
          TR::CompileIlGenRequest(entry->getMethodDetails()),
          entry->_checkpointInProgress
+#if defined(J9VM_OPT_JITSERVER)
+         , entry->_numPermanentLoaders
+#endif
          );
    if (TR::Options::getVerboseOption(TR_VerboseCompilationDispatch))
       TR_VerboseLog::writeLineLocked(TR_Vlog_DISPATCH,
@@ -9254,7 +9257,11 @@ TR::CompilationInfoPerThreadBase::wrappedCompile(J9PortLibrary *portLib, void * 
                p->trMemory(),
                p->_optimizationPlan,
                reloRuntime,
-               &target);
+               &target
+#if defined(J9VM_OPT_JITSERVER)
+               , p->_numPermanentLoaders
+#endif
+               );
 
 #if defined(J9VM_OPT_JITSERVER)
          // JITServer TODO: put info in optPlan so that compilation constructor can do this
