28f3ea3c14a54e7534bd87af8a85fd6694dd9b0a
Matthew Hall <matthew.hall3@outlook.com>
2025-08-14 17:43:08 -0400

    Change methodhandle default conflict method  fields
    Changes J9Method::extra for MethodHandle default conflict methods to
    J9_JIT_NEVER_TRANSLATE to fix crash in the JIT caused by assuming the
    default conflict method was compiled.
    
    Changes J9Method::methodRunAddress field to
    J9_BCLOOP_SEND_TARGET_DEFAULT_CONFLICT to use the normal default
    conflict handling in the vm.
    
    Removes special handling for default conflicts from MethodHandles since
    they now use the normal path.
    
    Dynamically modifies initialMethods.throwDefaultConflict on MH lookup so
    the error thrower has class and method info when it throws the error.
    
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/jcl/common/java_lang_invoke_MethodHandleNatives.cpp
+++ runtime/jcl/common/java_lang_invoke_MethodHandleNatives.cpp
@@ -1178,7 +1178,12 @@ Java_java_lang_invoke_MethodHandleNatives_resolve(
 							new_clazz = J9VM_J9CLASS_TO_HEAPCLASS(J9_CLASS_FROM_METHOD(method));
 							new_flags = flags;
 
-							/* Load special sendTarget to throw the exception during invocation */
+							/* Load special sendTarget to throw the exception during invocation
+							 * Change fields to match whats done in createramclass copyvtable to throw error properly
+							 */
+							vm->initialMethods.throwDefaultConflict->bytecodes = (U_8*)(J9_ROM_METHOD_FROM_RAM_METHOD(method) + 1);
+							vm->initialMethods.throwDefaultConflict->constantPool = callerClass->ramConstantPool;
+							vm->initialMethods.throwDefaultConflict->extra = (void *)((UDATA)method | J9_STARTPC_NOT_TRANSLATED);
 							target = JLONG_FROM_POINTER(vm->initialMethods.throwDefaultConflict);
 						} else {
 							goto done;
