e02d8a01c1a282d6f34dd31d78caa4f96bcc3636
lhu <linhu@ca.ibm.com>
2025-08-15 11:39:34 -0400

    Fix exception "committed argument cannot be less than 0"
    Previous PR #17107 target
    to prevent exception "committed argument cannot be less than 0" for
    MemoryPool "balanced-reserved" during collecting memoryUsage at the end
    of GC(balanced GC only), but there are two special cases haven't been
    covered.
    
    Fix case 1: maxHeapExpansionRegions could add on maxEdenChange and use
    _extensions->globalVLHGCStats._heapSizingData.edenRegionChange to notify
    heap resize to adjust heap size to match eden change, but it did not
    consider the case which eden size is bigger than free memory, so update
    _extensions->globalVLHGCStats._heapSizingData.edenRegionChange =
    OMR_MIN(maxHeapExpansionRegions, edenChangeWithSurvivorHeadroom +
    (intptr_t)(_edenRegionCount - freeRegions));
     for keeping free memory >= eden size after heap resize.
    
    Fix case 2: eden region can not be smaller than 1, when free region is 0
    eden region size can be 0.
    
    Add Assertion checks to confirm free size >= eden size.
    
    Update to correct freeTenure at the beginning of PGC
    
    should count out free eden size(for most case, free eden would be
    closed to 0, but in some cases free eden is significant, it might
    confuse the heap resize algorithm).
    
    Signed-off-by: lhu <linhu@ca.ibm.com>

--- runtime/gc_vlhgc/IncrementalGenerationalGC.cpp
+++ runtime/gc_vlhgc/IncrementalGenerationalGC.cpp
@@ -57,6 +57,7 @@
 #include "FinalizerSupport.hpp"
 #include "GlobalAllocationManager.hpp"
 #include "HeapRegionIteratorVLHGC.hpp"
+#include "HeapMemorySnapshot.hpp"
 #include "HeapStats.hpp"
 #include "IncrementalGenerationalGC.hpp"
 #include "InterRegionRememberedSet.hpp"
@@ -1010,6 +1011,7 @@ MM_IncrementalGenerationalGC::partialGarbageCollectPostWork(MM_EnvironmentVLHGC 
 
 	incrementRegionAges(env, _taxationThreshold, true);
 
+	verifyHeapSizing(env);
 	reportGCCycleFinalIncrementEnding(env);
 	reportGCIncrementEnd(env);
 	reportPGCEnd(env);
@@ -1239,6 +1241,8 @@ MM_IncrementalGenerationalGC::runGlobalGarbageCollection(MM_EnvironmentVLHGC *en
 	/* Global Collection - we max out ages on all live regions to remove them from the nursery collection set */
 	setRegionAgesToMax(env);
 	Assert_MM_true(0 == static_cast<MM_CycleStateVLHGC*>(env->_cycleState)->_vlhgcIncrementStats._copyForwardStats.getStallTime());
+	verifyHeapSizing(env);
+
 	reportGCCycleFinalIncrementEnding(env);
 	/* TODO: TEMPORARY: This is a temporary call that should be deleted once the new verbose format is in place */
 	/* NOTE: May want to move any tracepoints up into this routine */
@@ -1322,7 +1326,12 @@ MM_IncrementalGenerationalGC::preProcessPGCUsingCopyForward(MM_EnvironmentVLHGC 
 	 * NOTE: A second estimate for free tenure is later made - The lowest estimate for free tenure is used in heap sizing calculations
 	 */
 	_extensions->globalVLHGCStats._heapSizingData.freeTenure = freeMemoryForSurvivor;
-
+	/* in some case free eden size is significant, should not be counted as freeTenure */
+	uintptr_t allocatedSinceLastPGC = getAllocatedSinceLastPGC();
+	uintptr_t edenSize = getCurrentEdenSizeInBytes(NULL);
+	if (edenSize > allocatedSinceLastPGC) {
+		_extensions->globalVLHGCStats._heapSizingData.freeTenure -= edenSize - allocatedSinceLastPGC;
+	}
 	cycleState->_vlhgcIncrementStats._copyForwardStats._freeMemoryBefore = freeMemoryForSurvivor;
 	cycleState->_vlhgcIncrementStats._copyForwardStats._totalMemoryBefore = _extensions->getHeap()->getMemorySize();
 
@@ -2601,4 +2610,12 @@ MM_IncrementalGenerationalGC::getBytesScannedInGlobalMarkPhase()
 		bytesScanned = _persistentGlobalMarkPhaseState._vlhgcCycleStats._markStats._bytesScanned;
 	}
 	return bytesScanned;
+}
+
+void
+MM_IncrementalGenerationalGC::verifyHeapSizing(MM_EnvironmentVLHGC *env)
+{
+	Assert_GC_true_with_message3(env, (((MM_GlobalAllocationManagerTarok *)_extensions->globalAllocationManager)->getFreeRegionCount() >= _schedulingDelegate.getCurrentEdenSizeInRegions(env)),
+				"verifyHeapSizing freeRegionCount(%zu) is less than EdenRegionCount(%zu), allocatedSinceLastPGC=%zu bytes\n",
+				((MM_GlobalAllocationManagerTarok *)_extensions->globalAllocationManager)->getFreeRegionCount(), _schedulingDelegate.getCurrentEdenSizeInRegions(env), getAllocatedSinceLastPGC());
 }
