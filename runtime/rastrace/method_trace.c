cf92e13df690df62e41aefb6a3aa2c6fc65b775b
Aditi Srinivas <Aditi.Srini@ibm.com>
2025-09-04 11:25:33 -0700

    Address review commnets on eclipse-openj9/openj9/pull/20933

--- runtime/rastrace/method_trace.c
+++ runtime/rastrace/method_trace.c
@@ -172,12 +172,12 @@ traceMethodEnter(J9VMThread *thr, J9Method *method, void *receiverAddress, UDATA
 	J9UTF8* methodName = J9ROMMETHOD_NAME(J9_ROM_METHOD_FROM_RAM_METHOD(method));
 	J9UTF8* methodSignature = J9ROMMETHOD_SIGNATURE(J9_ROM_METHOD_FROM_RAM_METHOD(method));
 	UDATA modifiers = J9_ROM_METHOD_FROM_RAM_METHOD(method)->modifiers;
+	j9object_t receiver = J9_ARE_ANY_BITS_SET(modifiers, J9AccStatic) ? NULL : *(j9object_t *)receiverAddress;
 
 	if (0 != isCompiled) {
 		if (modifiers & J9AccStatic) {
 			Trc_MethodEntryCS(thr, J9UTF8_LENGTH(className), J9UTF8_DATA(className), J9UTF8_LENGTH(methodName), J9UTF8_DATA(methodName), J9UTF8_LENGTH(methodSignature), J9UTF8_DATA(methodSignature));
 		} else {
-			j9object_t receiver = *(j9object_t*)receiverAddress;
 			Trc_MethodEntryC(thr, J9UTF8_LENGTH(className), J9UTF8_DATA(className), J9UTF8_LENGTH(methodName), J9UTF8_DATA(methodName), J9UTF8_LENGTH(methodSignature), J9UTF8_DATA(methodSignature), receiver);
 		}
 	} else {
@@ -193,7 +193,6 @@ traceMethodEnter(J9VMThread *thr, J9Method *method, void *receiverAddress, UDATA
 				Trc_MethodArgumentsS(thr, traceMethodArguments(thr, methodSignature, (UDATA*)receiverAddress, buf, buf + sizeof(buf)));
 			}
 		} else {
-			j9object_t receiver = *(j9object_t*)receiverAddress;
 			J9Class* receiverClazz = J9OBJECT_CLAZZ(thr, receiver);
 			J9UTF8* receiverClassName = J9ROMCLASS_CLASSNAME(receiverClazz->romClass);
 
@@ -202,20 +201,10 @@ traceMethodEnter(J9VMThread *thr, J9Method *method, void *receiverAddress, UDATA
 			} else {
 				Trc_MethodEntry(thr, J9UTF8_LENGTH(className), J9UTF8_DATA(className), J9UTF8_LENGTH(methodName), J9UTF8_DATA(methodName), J9UTF8_LENGTH(methodSignature), J9UTF8_DATA(methodSignature), receiver);
 			}
-
-			if (0 != doParameters) {
-				Trc_MethodArguments(
-						thr,
-						(U_32)J9UTF8_LENGTH(receiverClassName),
-						J9UTF8_DATA(receiverClassName),
-						receiver,
-						traceMethodArguments(thr, methodSignature, (UDATA*)receiverAddress - 1, buf, buf + sizeof(buf)));
-			}
 		}
 	}
 
 	if (0 != doParameters) {
-		j9object_t receiver = J9_ARE_ANY_BITS_SET(modifiers, J9AccStatic) ? NULL : *(j9object_t *)receiverAddress;
 		J9Class *receiverClazz = NULL == receiver ? NULL : J9OBJECT_CLAZZ(thr, receiver);
 		J9JavaVM *vm = thr->javaVM;
 		const unsigned int maxStringLength = RAS_GLOBAL_FROM_JAVAVM(maxStringLength, vm);
@@ -239,16 +228,16 @@ traceMethodEnter(J9VMThread *thr, J9Method *method, void *receiverAddress, UDATA
 					sizeof(utf8Buffer),
 					&utf8Length);
 			if (NULL == utf8String) {
-				j9str_printf(outputString, RAS_MAX_STRING_LENGTH_LIMIT + 1, "<Memory allocation error>");
+				j9str_printf(outputString, sizeof(outputString), "<Memory allocation error>");
 			} else if (utf8Length > maxStringLength) {
-				j9str_printf(outputString, RAS_MAX_STRING_LENGTH_LIMIT + 1, "\"%.*s\"...", (U_32)maxStringLength, utf8String);
+				j9str_printf(outputString, sizeof(outputString), "\"%.*s\"...", (U_32)maxStringLength, utf8String);
 			} else {
-				j9str_printf(outputString, RAS_MAX_STRING_LENGTH_LIMIT + 1, "\"%.*s\"", (U_32)utf8Length, utf8String);
+				j9str_printf(outputString, sizeof(outputString), "\"%.*s\"", (U_32)utf8Length, utf8String);
 			}
-			Trc_MethodArgumentsStr(thr, receiver, J9UTF8_DATA(methodSignature), outputString);
 			if (utf8Buffer != utf8String) {
 				j9mem_free_memory(utf8String);
 			}
+			Trc_MethodArgumentsStr(thr, receiver, J9UTF8_DATA(methodSignature), outputString);
 		}
 	}
 }
