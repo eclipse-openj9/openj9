f1233039e5105b7e1f348eab580a62371c7c73e2
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2025-10-09 15:18:51 -0400

    Instance strict field support (part 1)
    This change includes support for strict instance fields
    except for the required stack map frame changes.
    
    A hash map is used to track that all strict instance fields
     are set prior to an <init> method's transition out of the
    early larval phase.
    
    Signed-off-by: Theresa Mammarella <Theresa.T.Mammarella@ibm.com>

--- runtime/oti/j9.h
+++ runtime/oti/j9.h
@@ -373,10 +373,7 @@ static const struct { \
 #define IS_CLASS_SIGNATURE(firstChar) ('L' == (firstChar))
 
 #if defined(J9VM_OPT_VALHALLA_STRICT_FIELDS)
-/* TODO Update the class version check if strict fields is released before value types. */
-#define J9ROMFIELD_IS_STRICT_FINAL(romClassOrClassfile, fieldModifiers) \
-	(J9_IS_CLASSFILE_OR_ROMCLASS_VALUETYPE_VERSION(romClassOrClassfile) \
-		&& J9_ARE_ALL_BITS_SET(fieldModifiers, J9AccStrictInit | J9AccFinal))
+#define J9ROMFIELD_IS_STRICT(fieldModifiers) J9_ARE_ALL_BITS_SET(fieldModifiers, J9AccStrictInit)
 #endif /* defined(J9VM_OPT_VALHALLA_STRICT_FIELDS) */
 
 #if defined(J9VM_OPT_CRIU_SUPPORT)
