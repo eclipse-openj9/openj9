2693aea6965b4f5d0276596cca431a8fd1b6cdd3
Henry Zongaro <zongaro@ca.ibm.com>
2025-10-21 12:22:00 -0700

    Disable requireNonNull simplifications
    The simplifyUnresolvedRequireNonNull and simplifyResolvedRequireNonNull
    transformations of CFG Simplifier have never been fully tested.  This
    change disables those transformations so that other transformations
    performed by CFG Simplifier that are known to be safe can be reenabled.
    
    The intention is that the "Requires non-null" transformations will be
    reenabled once they have been more thoroughly tested and any necessary
    bug fixes have been applied.
    
    Signed-off-by:  Henry Zongaro <zongaro@ca.ibm.com>

--- runtime/compiler/optimizer/J9CFGSimplifier.cpp
+++ runtime/compiler/optimizer/J9CFGSimplifier.cpp
@@ -74,9 +74,9 @@ bool J9::CFGSimplifier::simplifyUnresolvedRequireNonNull(bool needToDuplicateTre
    {
    OMR::Logger *log = comp()->log();
 
-   static char *disableSimplifyExplicitNULLTest = feGetEnv("TR_disableSimplifyExplicitNULLTest");
-   static char *disableSimplifyUnresolvedRequireNonNull = feGetEnv("TR_disableSimplifyUnresolvedRequireNonNull");
-   if (disableSimplifyExplicitNULLTest != NULL || disableSimplifyUnresolvedRequireNonNull != NULL)
+   static const char *enableSimplifyExplicitNULLTest = feGetEnv("TR_enableSimplifyExplicitNULLTest");
+   static const char *enableSimplifyUnresolvedRequireNonNull = feGetEnv("TR_enableSimplifyUnresolvedRequireNonNull");
+   if (enableSimplifyExplicitNULLTest == NULL && enableSimplifyUnresolvedRequireNonNull == NULL)
       return false;
 
    if (comp()->getOSRMode() == TR::involuntaryOSR)
@@ -237,9 +237,9 @@ bool J9::CFGSimplifier::simplifyResolvedRequireNonNull(bool needToDuplicateTree)
    {
    OMR::Logger *log = comp()->log();
 
-   static char *disableSimplifyExplicitNULLTest = feGetEnv("TR_disableSimplifyExplicitNULLTest");
-   static char *disableSimplifyResolvedRequireNonNull = feGetEnv("TR_disableSimplifyResolvedRequireNonNull");
-   if (disableSimplifyExplicitNULLTest != NULL || disableSimplifyResolvedRequireNonNull != NULL)
+   static const char *enableSimplifyExplicitNULLTest = feGetEnv("TR_enableSimplifyExplicitNULLTest");
+   static const char *enableSimplifyResolvedRequireNonNull = feGetEnv("TR_enableSimplifyResolvedRequireNonNull");
+   if (enableSimplifyExplicitNULLTest == NULL && enableSimplifyResolvedRequireNonNull == NULL)
       return false;
 
    if (comp()->getOSRMode() == TR::involuntaryOSR)
