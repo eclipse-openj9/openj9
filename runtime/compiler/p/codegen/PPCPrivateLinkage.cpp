4f7fb3b76c48928271d0cf20c37cf0e72e6f70d5
Matthew Hall <matthew.hall3@outlook.com>
2025-11-12 11:34:37 -0500

    no cmp
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
+++ runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
@@ -2965,7 +2965,7 @@ void J9::Power::PrivateLinkage::buildDirectCall(TR::Node *callNode,
       generateTrg1MemInstruction(cg(), TR::InstOpCode::Op_load, callNode, scratchReg,
                                  TR::MemoryReference::createWithDisplacement(cg(), j9MethodReg, offsetof(J9Method, extra), TR::Compiler->om.sizeofReferenceAddress()));
       generateTrg1Src1ImmInstruction(cg(), TR::InstOpCode::andi_r, callNode, scratchReg2, scratchReg, 1);
-      generateConditionalBranchInstruction(cg, TR::InstOpCode::bne, callNode, oolLabel, cndReg);
+      generateConditionalBranchInstruction(cg(), TR::InstOpCode::bne, callNode, oolLabel, cndReg);
      // generateTrg1Src1ImmInstruction(cg(), TR::InstOpCode::cmpi4, callNode, cndReg, scratchReg2, 0);
      // generateConditionalBranchInstruction(cg(), TR::InstOpCode::bne, callNode, oolLabel, cndReg);
 
