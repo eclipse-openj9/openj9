2d2494dedfb65746b9c16f1c2b96f112429d96fc
Matthew Hall <matthew.hall3@outlook.com>
2025-11-12 23:34:51 -0500

    test preserved
    Signed-off-by: Matthew Hall <matthew.hall3@outlook.com>

--- runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
+++ runtime/compiler/p/codegen/PPCPrivateLinkage.cpp
@@ -1910,6 +1910,7 @@ int32_t J9::Power::PrivateLinkage::buildPrivateLinkageArgs(TR::Node             
       {
       if (i == TR::RealRegister::gr11) {
          if (isJitDispatchJ9Method) {
+            printf("checking if reg is preserved for jit dispatch: %d\n", i);
             TR_ASSERT_FATAL(!properties.getPreserved((TR::RealRegister::RegNum)i), "should not preserve\n");
          }
       }
