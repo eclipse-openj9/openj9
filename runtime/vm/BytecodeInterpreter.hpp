6f76fd40be21cfd18cdce24af4f6a510733c4682
Theresa Mammarella <Theresa.T.Mammarella@ibm.com>
2025-11-14 09:21:03 -0500

    Restrict invokeinterface error sendMethod search to Java 8
    The change introduced in https://github.com/eclipse-openj9/openj9/pull/22698
    is not helpful for Java 11+. In Java 8 private methods can be resolved for
    invokeinterface but in Java 11+ they are skipped. The IAE for Java 11+ will
    be caught either during the bytecode execution or in
     throwUnsatisfiedLinkOrAbstractMethodError.
    
    Signed-off-by: Theresa Mammarella <Theresa.T.Mammarella@ibm.com>

--- runtime/vm/BytecodeInterpreter.hpp
+++ runtime/vm/BytecodeInterpreter.hpp
@@ -8322,6 +8322,7 @@ foundITableCache:
 						rc = GOTO_THROW_CURRENT_EXCEPTION;
 						goto done;
 					}
+#if JAVA_SPEC_VERSION < 11
 					if (J9_ARE_ALL_BITS_SET(romMethod->modifiers, J9AccAbstract)
 						&& !J9ROMCLASS_IS_INTERFACE(J9_CLASS_FROM_METHOD(_sendMethod)->romClass)
 					) {
@@ -8337,6 +8338,7 @@ foundITableCache:
 							NULL,
 							J9_LOOK_INTERFACE | J9_LOOK_NO_THROW | J9_LOOK_INVOKE_INTERFACE);
 					}
+#endif /* JAVA_SPEC_VERSION < 11 */
 					profileInvokeReceiver(REGISTER_ARGS, receiverClass, _literals, _sendMethod);
 					_pc += offset;
 					goto done;
