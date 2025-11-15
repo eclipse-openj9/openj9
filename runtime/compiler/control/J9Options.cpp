db6d34b23248506e527b4e2da86ebd596dddbbfe
Daryl Maier <maier@ca.ibm.com>
2025-11-15 06:13:19 -0500

    Support factory constructor for TR::Options
    Signed-off-by: Daryl Maier <maier@ca.ibm.com>

--- runtime/compiler/control/J9Options.cpp
+++ runtime/compiler/control/J9Options.cpp
@@ -3972,3 +3972,9 @@ J9::Options::resetFSD(J9JavaVM *vm, J9VMThread *vmThread, bool &doAOT)
    return fsdStatusJIT;
    }
 #endif /* defined(J9VM_OPT_CRIU_SUPPORT) */
+
+void
+J9::Options::initialize()
+   {
+   self()->OMR::OptionsConnector::initialize();
+   }
