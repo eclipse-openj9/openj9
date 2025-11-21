1e9b424e08625fd19fbda7b112878c62d90c3d73
Devin Papineau <devin@ajdmp.ca>
2025-11-21 09:41:48 -0500

    Add const provenance edge for MCS epoch only if one was found

--- runtime/compiler/env/VMJ9.cpp
+++ runtime/compiler/env/VMJ9.cpp
@@ -4811,10 +4811,10 @@ TR::KnownObjectTable::Index TR_J9VMBase::mutableCallSiteEpoch(
    if (mh != 0)
       {
       result = knot->getOrCreateIndex(mh);
-      }
 
-   J9::ConstProvenanceGraph *cpg = comp->constProvenanceGraph();
-   cpg->addEdge(cpg->knownObject(mcs), cpg->knownObject(result));
+      J9::ConstProvenanceGraph *cpg = comp->constProvenanceGraph();
+      cpg->addEdge(cpg->knownObject(mcs), cpg->knownObject(result));
+      }
 
    return result;
    }
