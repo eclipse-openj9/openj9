fae41c2dacf93c460ffb46f5902bf8f50e136fb0
Rahil Shah <rahil@ca.ibm.com>
2026-03-17 13:47:52 -0400

    Refactor JProfilingValue
    Changes in this commit refactors the JProfilingValue code to achieve following
    objects.
    1. Fix a bug where incorrect bytecode info can be used to construct a table for
       value profiling
    2. Utilize list to capture the list of tree-tops for value profiling place
       holder calls in the first tree-top walk to use them in lowering calls
       instead of doing tree-top walk again.
    3. Refactor addProfilingTrees code to get it ready for profiling in low
       optimization level.
    
    Signed-off-by: Rahil Shah <rahil@ca.ibm.com>

--- runtime/compiler/optimizer/JProfilingValue.hpp
+++ runtime/compiler/optimizer/JProfilingValue.hpp
@@ -47,12 +47,12 @@ public:
      * Identify place holder calls to jProfileValueSymbol and jProfileValueWithNullCHKSymbol, lowering them
      * into the fast, slow and helper paths.
      */
-    void lowerCalls();
+    void lowerCalls(TR::list<TR::TreeTop *> &valueProfilingPlaceHolderCalls);
     /**
      * Clean up duplicate profiling candidates and add profiling placeholder calls
      * for profiling target of virtual call dispatch and instanceOf/checkCast.
      */
-    void cleanUpAndAddProfilingCandidates();
+    void cleanUpAndAddProfilingCandidates(TR::list<TR::TreeTop *> &valueProfilingPlaceHolderCalls);
     /**
      * Examines node to identify profiling candidate and add place holder calls for profiling it.
      * This routine checks node and it's children for mainly two type of nodes.
@@ -66,12 +66,12 @@ public:
      * Blocks
      * @param checklist A Node checklist to make sure while recursively examining node, we do not examine node multiple
      * times.
+     * @param valueProfilingPlaceHolderCalls A list holding value profiling place holder tree tops
      */
-    void performOnNode(TR::Node *node, TR::TreeTop *cursor, TR_BitVector *alreadyProfiledValues,
-        TR::NodeChecklist *checklist);
+    void performOnNode(TR::Node *node, TR::TreeTop *cursor, TR_BitVector *alreadyProfiledValues, TR::NodeChecklist *checklist, TR::list<TR::TreeTop *> &valueProfilingPlaceHolderCalls);
 
-    static bool addProfilingTrees(TR::Compilation *comp, TR::TreeTop *insertionPoint, TR::Node *value,
-        TR_AbstractHashTableProfilerInfo *table, bool addNullCheck = false, bool extendBlocks = true,
+    static TR::TreeTop * addProfilingTrees(TR::Compilation *comp, TR::TreeTop *insertionPoint, TR::Node *value,
+        TR_AbstractHashTableProfilerInfo *table, TR::TreeTop *lastTreeTop = NULL, bool extendBlocks = true,
         bool trace = false);
 
 private:
