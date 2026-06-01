b565c42efacf55c8c5858d1e86eff779e653575d
jimmyk <jimmyk@ca.ibm.com>
2026-06-01 16:29:53 -0400

    Fix location of AI attribution statement
    Signed-off-by: jimmyk <jimmyk@ca.ibm.com>

--- runtime/compiler/p/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/p/codegen/J9TreeEvaluator.cpp
@@ -18,9 +18,8 @@
  * [2] https://openjdk.org/legal/assembly-exception.html
  *
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0-only WITH Classpath-exception-2.0 OR GPL-2.0-only WITH OpenJDK-assembly-exception-1.0
+ * Assisted-by: IBM Bob
  *******************************************************************************/
-
-// Assisted-by: IBM Bob
 
 #include <limits.h>
 #include <math.h>
