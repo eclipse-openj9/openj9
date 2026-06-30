f2e240b946c4a024e8ce5adb9755da4998c500d5
konark24117 <konarkshah2010@gmail.com>
2026-06-30 13:00:52 -0400

    fix

--- test/functional/CacheManagement/src/tests/sharedclasses/options/TestControlDirCacheDir02.java
+++ test/functional/CacheManagement/src/tests/sharedclasses/options/TestControlDirCacheDir02.java
@@ -64,7 +64,7 @@ public class TestControlDirCacheDir02 extends TestUtils {
 		  checkForSuccessfulNonPersistentCacheOpenMessage("Foo");
 		  
 		  try {
-			Thread.sleep(500);
+			Thread.sleep(100);
 		  } catch (InterruptedException e) {
 			e.printStackTrace();
 		  }
