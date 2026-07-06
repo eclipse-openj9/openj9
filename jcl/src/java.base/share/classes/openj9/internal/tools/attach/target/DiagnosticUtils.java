dc8b90b8fd0a62d23415588c94715cbfe2ee17a6
Jason Feng <fengj@ca.ibm.com>
2026-07-06 11:58:01 -0400

    JFR duration timer thread should be daemon
    A daemon timer thread won't prevent JVM shutdown.
    
    Signed-off-by: Jason Feng <fengj@ca.ibm.com>

--- jcl/src/java.base/share/classes/openj9/internal/tools/attach/target/DiagnosticUtils.java
+++ jcl/src/java.base/share/classes/openj9/internal/tools/attach/target/DiagnosticUtils.java
@@ -560,7 +560,7 @@ public class DiagnosticUtils {
 					}
 					VM.startJFR();
 					if (duration > 0) {
-						Timer timer = new Timer();
+						Timer timer = new Timer(true);
 						TimerTask jfrDumpTask = new TimerTask() {
 							public void run() {
 								if (VM.isJFRRecordingStarted()) {
